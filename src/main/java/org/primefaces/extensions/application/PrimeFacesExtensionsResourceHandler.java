a18c2a90a63195155b74ced611d787b297cc2b37
Thomas Andraschko <andraschko.thomas@gmail.com>
2011-06-07 19:19:42 +0000

    next version

--- src/main/java/org/primefaces/extensions/application/PrimeFacesExtensionsResourceHandler.java
+++ src/main/java/org/primefaces/extensions/application/PrimeFacesExtensionsResourceHandler.java
@@ -21,7 +21,7 @@ import javax.faces.application.ResourceHandlerWrapper;
 
 public class PrimeFacesExtensionsResourceHandler extends ResourceHandlerWrapper {
 
-	public static final String VERSION = "0.1-SNAPSHOT";
+	public static final String VERSION = "0.2-SNAPSHOT";
 	public static final String LIBRARY = "primefaces-extensions";
 
 	private ResourceHandler resourceHandler;
