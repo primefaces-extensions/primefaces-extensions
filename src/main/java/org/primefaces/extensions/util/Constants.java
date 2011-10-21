9bceec8a4e9a869e834386847ee0ef6a200b2188
Thomas Andraschko <andraschko.thomas@gmail.com>
2011-10-21 20:01:35 +0000

    Updated version

--- src/main/java/org/primefaces/extensions/util/Constants.java
+++ src/main/java/org/primefaces/extensions/util/Constants.java
@@ -27,6 +27,6 @@ package org.primefaces.extensions.util;
  */
 public class Constants {
 
-	public static final String VERSION = "0.2-SNAPSHOT";
+	public static final String VERSION = "0.2.0-SNAPSHOT";
 	public static final String LIBRARY = "primefaces-extensions";
 }
