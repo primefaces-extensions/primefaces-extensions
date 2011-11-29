29f4a8a5a13e97284c2e21a80e032f4680dbda1e
Thomas Andraschko <andraschko.thomas@gmail.com>
2011-11-29 13:49:49 +0000

    cosmetics

--- src/main/java/org/primefaces/extensions/component/tooltip/TooltipRenderer.java
+++ src/main/java/org/primefaces/extensions/component/tooltip/TooltipRenderer.java
@@ -20,7 +20,6 @@ package org.primefaces.extensions.component.tooltip;
 
 import java.io.IOException;
 
-import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
