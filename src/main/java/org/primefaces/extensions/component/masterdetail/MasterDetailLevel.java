5c9b003e5d97f97b5244daf372cee206f4740fb6
Thomas Andraschko <andraschko.thomas@gmail.com>
2011-11-29 13:58:25 +0000

    cosmetics

--- src/main/java/org/primefaces/extensions/component/masterdetail/MasterDetailLevel.java
+++ src/main/java/org/primefaces/extensions/component/masterdetail/MasterDetailLevel.java
@@ -114,7 +114,9 @@ public class MasterDetailLevel extends UIComponentBase {
 			final String cname = this.getClass().getName();
 			if (cname != null && cname.startsWith(OPTIMIZED_PACKAGE)) {
 				setAttributes = new ArrayList<String>(6);
-				this.getAttributes().put("javax.faces.component.UIComponentBase.attributesThatAreSet", setAttributes);
+				this.getAttributes().put(
+						"javax.faces.component.UIComponentBase.attributesThatAreSet",
+						setAttributes);
 			}
 		}
 
