930b09ff8a86256068e350b862b450476a0865ad
Oleg Varaksin <ovaraksin@googlemail.com>
2011-12-14 22:00:46 +0000

    Improved LocaleConverter

--- src/main/java/org/primefaces/extensions/converter/LocaleConverter.java
+++ src/main/java/org/primefaces/extensions/converter/LocaleConverter.java
@@ -18,35 +18,89 @@
 
 package org.primefaces.extensions.converter;
 
+import java.util.Locale;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
-import org.apache.commons.lang.LocaleUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * {@link Converter} which converts a string to a {@link java.util.Locale} an vice-versa.
  *
- * @author Thomas Andraschko / last modified by $Author$
+ * @author  Thomas Andraschko / last modified by $Author$
  * @version $Revision$
- * @since 0.2
+ * @since   0.2
  */
 @FacesConverter(value = "org.primefaces.extensions.converter.LocaleConverter")
 public class LocaleConverter implements Converter {
 
 	@Override
-	public Object getAsObject(final FacesContext context, final UIComponent component, final String value) {
-		try {
-			return LocaleUtils.toLocale(value);
-		} catch (IllegalArgumentException e) {
-			throw new ConverterException(e.getMessage(), e);
+	public Object getAsObject(final FacesContext fc, final UIComponent component, final String value) {
+		if (StringUtils.isBlank(value)) {
+			return fc.getApplication().getDefaultLocale();
 		}
+
+		return getLocaleObject(value);
 	}
 
 	@Override
-	public String getAsString(final FacesContext context, final UIComponent component, final Object value) {
-		return value.toString();
+	public String getAsString(final FacesContext fc, final UIComponent component, final Object value) {
+		if (value == null) {
+			Locale defaultLocale = fc.getApplication().getDefaultLocale();
+			if (defaultLocale == null) {
+				return null;
+			}
+
+			return getLocaleString(defaultLocale);
+		}
+
+		if (value instanceof String) {
+			return (String) value;
+		} else if (value instanceof Locale) {
+			return getLocaleString((Locale) value);
+		} else {
+			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR,
+			                                              "Wrong type: '" + value.getClass().getSimpleName()
+			                                              + "' is not 'Locale'.", StringUtils.EMPTY));
+		}
+	}
+
+	public static Locale getLocaleObject(final String strLocale) {
+		final String[] parts = strLocale.replace('-', '_').split("_");
+		if (parts.length == 0
+		    || !parts[0].matches("[a-zA-Z]{2,2}")
+		    || (parts.length > 1 && parts[1].length() != 0 && !parts[1].matches("[a-zA-Z]{2,2}"))) {
+			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR,
+			                                              "'" + strLocale + "' does not represent a valid locale",
+			                                              StringUtils.EMPTY));
+		}
+
+		switch (parts.length) {
+		case 3:
+			return new Locale(parts[0], parts[1], parts[2]);
+
+		case 2:
+			return new Locale(parts[0], parts[1]);
+
+		case 1:
+			return new Locale(parts[0]);
+
+		default:
+			return new Locale(parts[0], parts[1], StringUtils.join(ArrayUtils.subarray(parts, 2, parts.length), '_'));
+		}
+	}
+
+	public static String getLocaleString(final Locale locale) {
+		if (StringUtils.isBlank(locale.getCountry())) {
+			return locale.getLanguage();
+		}
+
+		return locale.getLanguage() + "_" + locale.getCountry();
 	}
 }
