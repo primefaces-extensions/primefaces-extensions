d50a37ae51b1eeb3f00e5fa219a5467985b0289a
Oleg Varaksin <ovaraksin@googlemail.com>
2012-01-30 19:29:29 +0000

    Moved blockUI script to primefaces-extensions.jar because Timeline uses it too

--- src/main/java/org/primefaces/extensions/component/blockui/BlockUI.java
+++ src/main/java/org/primefaces/extensions/component/blockui/BlockUI.java
@@ -39,8 +39,7 @@ import org.primefaces.component.api.Widget;
  */
 @ResourceDependencies({
                           @ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
-                          @ResourceDependency(library = "primefaces-extensions", name = "primefaces-extensions.js"),
-                          @ResourceDependency(library = "primefaces-extensions", name = "blockui/blockui.js")
+                          @ResourceDependency(library = "primefaces-extensions", name = "primefaces-extensions.js")
                       })
 public class BlockUI extends UIComponentBase implements Widget {
 
