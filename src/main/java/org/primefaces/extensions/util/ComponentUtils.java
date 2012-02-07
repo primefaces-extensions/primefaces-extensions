957ed844c579fe547178cd106900605cd12e67e8
Oleg Varaksin <ovaraksin@googlemail.com>
2012-02-07 20:38:48 +0000

    Added escapeText() and fixed findTarget()

--- src/main/java/org/primefaces/extensions/util/ComponentUtils.java
+++ src/main/java/org/primefaces/extensions/util/ComponentUtils.java
@@ -115,7 +115,7 @@ public class ComponentUtils extends org.primefaces.util.ComponentUtils {
 				throw new FacesException("Cannot find component '" + forValue + "'.");
 			}
 
-			return ComponentUtils.escapeJQueryId(forComponent.getClientId(context));
+			return escapeJQueryId(forComponent.getClientId(context));
 		}
 
 		if (attachable instanceof EnhancedAttachable) {
@@ -124,14 +124,14 @@ public class ComponentUtils extends org.primefaces.util.ComponentUtils {
 
 			if (forSelector != null) {
 				if (forSelector.startsWith("#")) {
-					return ComponentUtils.escapeComponentId(forSelector);
-				}
-
-				return forSelector;
+					return escapeComponentId(forSelector);
+				} else {
+                    return escapeText(forSelector);
+                }
 			}
 		}
 
-		return ComponentUtils.escapeJQueryId(component.getParent().getClientId(context));
+		return escapeJQueryId(component.getParent().getClientId(context));
 	}
 
 	public static void addComponentResource(final FacesContext context, final String name) {
@@ -151,4 +151,63 @@ public class ComponentUtils extends org.primefaces.util.ComponentUtils {
 
         context.getViewRoot().addComponentResource(context, componentResource, target);
     }
+    
+    /**
+     * Duplicate code from json-simple project under apache license
+     * http://code.google.com/p/json-simple/source/browse/trunk/src/org/json/simple/JSONValue.java
+     * 
+     * @param text original text as string
+     * @return String escaped text as string to be used as JSON value
+     */
+    public static String escapeText(final String text) {
+        if(text == null) {
+            return null;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            switch (ch) {
+                case '"':
+                    sb.append("\\\"");
+                    break;
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                case '/':
+                    sb.append("\\/");
+                    break;
+                default:
+                    //Reference: http://www.unicode.org/versions/Unicode5.1.0/
+                    if((ch >= '\u0000' && ch <= '\u001F') || (ch >= '\u007F' && ch <= '\u009F') || (ch >= '\u2000' && ch <= '\u20FF')) {
+                        String ss = Integer.toHexString(ch);
+                        sb.append("\\u");
+                        for (int k = 0; k < 4 - ss.length(); k++) {
+                            sb.append('0');
+                        }
+                        sb.append(ss.toUpperCase());
+                    } else {
+                        sb.append(ch);
+                    }
+            }
+        }
+                
+        return sb.toString();
+    }    
 }
