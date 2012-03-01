37296d76ac8eff18190ff85ee354e4259385e965
Oleg Varaksin <ovaraksin@googlemail.com>
2012-03-01 19:44:40 +0000

    TimePicker and related stuff

--- src/main/java/org/primefaces/extensions/util/MessageUtils.java
+++ src/main/java/org/primefaces/extensions/util/MessageUtils.java
@@ -23,9 +23,7 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-import javax.faces.FactoryFinder;
 import javax.faces.application.Application;
-import javax.faces.application.ApplicationFactory;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -43,13 +41,6 @@ public class MessageUtils {
 
 	public static final String FACES_MESSAGES = "javax.faces.Messages";
 
-	/**
-	 * Returns the <code>label</code> property from the specified component.
-	 *
-	 * @param  context   - the <code>FacesContext</code> for the current request
-	 * @param  component - the component of interest
-	 * @return the label, if any, of the component
-	 */
 	public static Object getLabel(final FacesContext context, final UIComponent component) {
 		Object label = component.getAttributes().get("label");
 		if (label == null || (label instanceof String && StringUtils.isEmpty((String) label))) {
@@ -64,29 +55,14 @@ public class MessageUtils {
 		return label;
 	}
 
-	/**
-	 * Creates and returns a FacesMessage for the specified Locale, message key and parameter(s).
-	 *
-	 * @param  locale - the target <code>Locale</code>
-	 * @param  key    messageId - the key of the message in the resource bundle
-	 * @param  params - substittion parameters
-	 * @return a localized <code>FacesMessage</code> with the severity of FacesMessage.SEVERITY_ERROR
-	 * @throws NullPointerException DOCUMENT_ME
-	 */
 	public static FacesMessage getMessage(final Locale locale, final String key, final Object... params) {
 		String summary = null;
 		String detail = null;
 		ResourceBundle bundle;
 		String bundleName;
 
-		final Application application;
 		final FacesContext context = FacesContext.getCurrentInstance();
-		if (context != null) {
-			application = context.getApplication();
-		} else {
-			ApplicationFactory aFactory = (ApplicationFactory) FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
-			application = aFactory.getApplication();
-		}
+		final Application application = context.getApplication();
 
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
 		if (loader == null) {
@@ -96,7 +72,7 @@ public class MessageUtils {
 		// try to find message in user provided bundle
 		bundleName = application.getMessageBundle();
 		if (bundleName != null) {
-			bundle = ResourceBundle.getBundle(bundleName, locale, loader);
+			bundle = ResourceBundle.getBundle(bundleName, (locale != null ? locale : context.getViewRoot().getLocale()), loader);
 			if (bundle != null) {
 				try {
 					summary = bundle.getString(key);
@@ -109,7 +85,8 @@ public class MessageUtils {
 
 		if (summary == null) {
 			// try to find message in JSF standard provided bundle
-			bundle = ResourceBundle.getBundle(FACES_MESSAGES, locale, loader);
+			bundle =
+			    ResourceBundle.getBundle(FACES_MESSAGES, (locale != null ? locale : context.getViewRoot().getLocale()), loader);
 			if (bundle == null) {
 				throw new NullPointerException();
 			}
@@ -131,10 +108,14 @@ public class MessageUtils {
 		}
 
 		if (summary != null) {
-			// create and return a message
 			return new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, (detail != null ? detail : StringUtils.EMPTY));
 		}
 
-		return null;
+		return new FacesMessage(FacesMessage.SEVERITY_ERROR, "???" + key + "???", (detail != null ? detail : StringUtils.EMPTY));
+	}
+
+	public static FacesMessage getMessage(final String key, final Object... params) {
+		// let current locale to be calculated
+		return getMessage(null, key, params);
 	}
 }
