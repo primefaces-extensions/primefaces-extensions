7d4644ac8c2e5392af173d18b4454b53e36c5373
Thomas Andraschko <andraschko.thomas@gmail.com>
2012-03-21 21:24:39 +0000

    cosmetics

--- src/main/java/org/primefaces/extensions/component/importconstants/ImportConstantsTagHandler.java
+++ src/main/java/org/primefaces/extensions/component/importconstants/ImportConstantsTagHandler.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2011 PrimeFaces Extensions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+
 package org.primefaces.extensions.component.importconstants;
 
 import java.io.IOException;
@@ -18,6 +36,13 @@ import javax.faces.view.facelets.TagAttribute;
 import javax.faces.view.facelets.TagConfig;
 import javax.faces.view.facelets.TagHandler;
 
+/**
+ * {@link TagHandler} for the <code>importConstants</code> component.
+ *
+ * @author Thomas Andraschko / last modified by $Author$
+ * @version $Revision$
+ * @since 0.5
+ */
 public class ImportConstantsTagHandler extends TagHandler {
 
 	private static final Map<Class<?>, Map<String, Object>> CACHE = new Hashtable<Class<?>, Map<String, Object>>();
