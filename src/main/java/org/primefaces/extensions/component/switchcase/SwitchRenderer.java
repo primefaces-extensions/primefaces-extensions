03ce31b65bab09ca9c8e4557d6584df3056492ac
Thomas Andraschko <andraschko.thomas@gmail.com>
2012-09-18 18:39:13 +0000

    cosmetics

--- src/main/java/org/primefaces/extensions/component/switchcase/SwitchRenderer.java
+++ src/main/java/org/primefaces/extensions/component/switchcase/SwitchRenderer.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2011-2012 PrimeFaces Extensions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id: $
+ */
+
 package org.primefaces.extensions.component.switchcase;
 
 import java.io.IOException;
@@ -7,8 +25,16 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
+import org.primefaces.extensions.component.keyfilter.KeyFilter;
 import org.primefaces.renderkit.CoreRenderer;
 
+/**
+ * Renderer for the {@link Switch} component.
+ *
+ * @author Michael Gmeiner / last modified by $Author: $
+ * @version $Revision: $
+ * @since 0.6
+ */
 public class SwitchRenderer extends CoreRenderer {
 	
 	@Override
