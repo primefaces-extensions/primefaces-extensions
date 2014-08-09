1d851da7872aaa6bc28407bab0b383f404a0a8a0
tandraschko <tandraschko@apache.org>
2014-08-09 14:02:31 +0200

    CKEditor fixes

--- src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/widget.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/widget.js
@@ -5,48 +5,53 @@
  * @returns {string} The faces resource URL.
  */
 CKEDITOR_GETURL = function(resource) {
-	var facesResource;
+    var facesResource;
 
-	//do not resolve
-	if (resource.indexOf('?resolve=false') !== -1) {
-		facesResource = resource.replace('?resolve=false', '');
-	} else {
-		//already wrapped?
-		var libraryVersionIndex = resource.indexOf('v=' + PrimeFacesExt.getPrimeFacesExtensionsVersion());
-		if (libraryVersionIndex !== -1) {
-			//look for appended resource
-			var appendedResource = resource.substring(libraryVersionIndex + ('v=' + PrimeFacesExt.getPrimeFacesExtensionsVersion()).length);
-	
-			if (appendedResource.length > 0) {
-				//remove append resource from url
-				facesResource = resource.substring(0, resource.length - appendedResource.length);
-	
-				var resourceIdentiferPosition = facesResource.indexOf(PrimeFacesExt.RESOURCE_IDENTIFIER);
-				
-				if (PrimeFacesExt.isExtensionMapping()) {
-					var extensionMappingPosition = facesResource.lastIndexOf('.' + PrimeFacesExt.getResourceUrlExtension());
-	
-					//extract resource
-					var extractedResource = facesResource.substring(resourceIdentiferPosition + PrimeFacesExt.RESOURCE_IDENTIFIER.length, extensionMappingPosition);
-	
-					facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource(extractedResource + appendedResource);
-				} else {
-					var questionMarkPosition = facesResource.indexOf('?');
-	
-					//extract resource
-					var extractedResource = facesResource.substring(resourceIdentiferPosition + PrimeFacesExt.RESOURCE_IDENTIFIER.length, questionMarkPosition);
-	
-					facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource(extractedResource + appendedResource);
-				}
-			} else {
-				facesResource = resource;
-			}
-		} else {
-			facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/' + resource);
-		}
-	}
-	
-	return facesResource;
+    //do not resolve
+    if (resource.indexOf('?resolve=false') !== -1) {
+        facesResource = resource.replace('?resolve=false', '');
+    } else {
+        //already wrapped?
+        var libraryVersionIndex = resource.indexOf('v=' + PrimeFacesExt.getPrimeFacesExtensionsVersion());
+        if (libraryVersionIndex !== -1) {
+            //look for appended resource
+            var appendedResource = resource.substring(libraryVersionIndex + ('v=' + PrimeFacesExt.getPrimeFacesExtensionsVersion()).length);
+
+            if (appendedResource.length > 0) {
+                //remove append resource from url
+                facesResource = resource.substring(0, resource.length - appendedResource.length);
+
+                var resourceIdentiferPosition = facesResource.indexOf(PrimeFacesExt.RESOURCE_IDENTIFIER);
+
+                if (PrimeFacesExt.isExtensionMapping()) {
+                    var extensionMappingPosition = facesResource.lastIndexOf('.' + PrimeFacesExt.getResourceUrlExtension());
+
+                    //extract resource
+                    var extractedResource = facesResource.substring(resourceIdentiferPosition + PrimeFacesExt.RESOURCE_IDENTIFIER.length, extensionMappingPosition);
+
+                    facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource(extractedResource + appendedResource);
+                } else {
+                    var questionMarkPosition = facesResource.indexOf('?');
+
+                    //extract resource
+                    var extractedResource = facesResource.substring(resourceIdentiferPosition + PrimeFacesExt.RESOURCE_IDENTIFIER.length, questionMarkPosition);
+
+                    facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource(extractedResource + appendedResource);
+                }
+            } else {
+                facesResource = resource;
+            }
+        } else {
+            if (resource.indexOf(PrimeFacesExt.RESOURCE_IDENTIFIER) === -1) {
+                facesResource = PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/' + resource);
+            }
+            else {
+                facesResource = resource;
+            }
+        }
+    }
+
+    return facesResource;
 };
 
 /**
@@ -65,72 +70,72 @@ PrimeFacesExt.widget.CKEditor = PrimeFaces.widget.DeferredWidget.extend({
 		this._super(cfg);
 
 	    this.instance = null;
-	
-		this.options = {};
-		//add widget to ckeditor config, this is required for the save event
-		this.options.widgetVar = this.cfg.widgetVar;
-	
-		if (this.cfg.skin) {
-			this.options.skin = this.cfg.skin;
-		}
-		if (this.cfg.width) {
-			this.options.width = this.cfg.width;
-		}
-		if (this.cfg.height) {
-			this.options.height = this.cfg.height;
-		}
-		if (this.cfg.theme) {
-			this.options.theme = this.cfg.theme;
-		}
-		if (this.cfg.toolbar) {
-			if (!(this.cfg.toolbar instanceof Array)) {
-				this.options.toolbar = eval(this.cfg.toolbar);
-			} else {
-				this.options.toolbar = this.cfg.toolbar;
-			}
-		}
-		if (this.cfg.readOnly) {
-			this.options.readOnly = this.cfg.readOnly;
-		}
-		if (this.cfg.interfaceColor) {
-			this.options.uiColor = this.cfg.interfaceColor;
-		}
-		if (this.cfg.language) {
-			this.options.language = this.cfg.language;
-		}
-		if (this.cfg.defaultLanguage) {
-			this.options.defaultLanguage = this.cfg.defaultLanguage;
-		}
-		if (this.cfg.contentsCss) {
-			this.options.contentsCss = this.cfg.contentsCss;
-		}
-		if (this.cfg.customConfig) {
-			this.options.customConfig = this.cfg.customConfig + "?resolve=false";
-		}
-	
-		//check if ckeditor is already included
-		if (!$.fn.ckeditor) {
-			var ckEditorScriptURI =
-				PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/ckeditor.js');
-			
-			var jQueryAdapterScriptURI =
-				PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/adapters/jquery.js');		
-	
-			//load ckeditor
-			PrimeFaces.getScript(ckEditorScriptURI, $.proxy(function(data, textStatus) {
-	
-				//load jquery adapter
-				PrimeFaces.getScript(jQueryAdapterScriptURI, $.proxy(function(data, textStatus) {
-	
-					this.renderDeferred();
 
-				}, this));
-	
-			}, this), true);
-	
-		} else {
-			this.renderDeferred();
-		}
+            this.options = {};
+            //add widget to ckeditor config, this is required for the save event
+            this.options.widgetVar = this.cfg.widgetVar;
+
+            if (this.cfg.skin) {
+                this.options.skin = this.cfg.skin;
+            }
+            if (this.cfg.width) {
+                this.options.width = this.cfg.width;
+            }
+            if (this.cfg.height) {
+                this.options.height = this.cfg.height;
+            }
+            if (this.cfg.theme) {
+                this.options.theme = this.cfg.theme;
+            }
+            if (this.cfg.toolbar) {
+                if (!(this.cfg.toolbar instanceof Array)) {
+                    this.options.toolbar = eval(this.cfg.toolbar);
+                } else {
+                    this.options.toolbar = this.cfg.toolbar;
+                }
+            }
+            if (this.cfg.readOnly) {
+                this.options.readOnly = this.cfg.readOnly;
+            }
+            if (this.cfg.interfaceColor) {
+                this.options.uiColor = this.cfg.interfaceColor;
+            }
+            if (this.cfg.language) {
+                this.options.language = this.cfg.language;
+            }
+            if (this.cfg.defaultLanguage) {
+                this.options.defaultLanguage = this.cfg.defaultLanguage;
+            }
+            if (this.cfg.contentsCss) {
+                this.options.contentsCss = this.cfg.contentsCss;
+            }
+            if (this.cfg.customConfig) {
+                this.options.customConfig = this.cfg.customConfig + "?resolve=false";
+            }
+
+            //check if ckeditor is already included
+            if (!$.fn.ckeditor) {
+                var ckEditorScriptURI =
+                    PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/ckeditor.js');
+
+                var jQueryAdapterScriptURI =
+                    PrimeFacesExt.getPrimeFacesExtensionsCompressedResource('/ckeditor/adapters/jquery.js');		
+
+                //load ckeditor
+                PrimeFaces.getScript(ckEditorScriptURI, $.proxy(function(data, textStatus) {
+
+                    //load jquery adapter
+                    PrimeFaces.getScript(jQueryAdapterScriptURI, $.proxy(function(data, textStatus) {
+
+                            this.renderDeferred();
+
+                    }, this));
+
+                }, this), true);
+
+            } else {
+                    this.renderDeferred();
+            }
 	},
 
 	/**
@@ -140,25 +145,25 @@ PrimeFacesExt.widget.CKEditor = PrimeFaces.widget.DeferredWidget.extend({
 	 * @private
 	 */
 	_render : function() {
-		if (!this.instance) {
-			//overwrite save button
-			this.overwriteSaveButton();
-	
-			//remove old instances if required
-			var oldInstance = CKEDITOR.instances[this.id];
-			if (oldInstance) {
-				try {
-					oldInstance.destroy(true);
-				} catch (err) {
-					if (window.console && console.log) {
-						console.log('CKEditor throwed a error while destroying the old instance: ' + err);
-					}
-				}
-			}
-	
-			//initialize ckeditor after all resources were loaded
-			this.jq.ckeditor($.proxy(function() { this.initialized(); }, this), this.options);
-		}
+            if (!this.instance) {
+                //overwrite save button
+                this.overwriteSaveButton();
+
+                //remove old instances if required
+                var oldInstance = CKEDITOR.instances[this.id];
+                if (oldInstance) {
+                    try {
+                        oldInstance.destroy(true);
+                    } catch (err) {
+                        if (window.console && console.log) {
+                            console.log('CKEditor throwed a error while destroying the old instance: ' + err);
+                        }
+                    }
+                }
+
+                //initialize ckeditor after all resources were loaded
+                this.jq.ckeditor($.proxy(function() { this.initialized(); }, this), this.options);
+            }
 	},
 
 	/**
@@ -194,34 +199,34 @@ PrimeFacesExt.widget.CKEditor = PrimeFaces.widget.DeferredWidget.extend({
 		};
 	},
 
-	/**
-	 * This method will be called when the CKEditor was initialized. 
-	 *
-	 * @private
-	 */
-	initialized : function() {
-		//get instance
-		this.instance = this.jq.ckeditorGet();
-		
-		//fire initialize event
-		this.fireEvent('initialize');
-		
-		//register blur and focus event
-	    this.instance.on('blur', $.proxy(function() { this.fireEvent('blur'); }, this));
-	    this.instance.on('focus', $.proxy(function() { this.fireEvent('focus'); }, this));
+    /**
+     * This method will be called when the CKEditor was initialized. 
+     *
+     * @private
+     */
+    initialized : function() {
+        //get instance
+        this.instance = this.jq.ckeditorGet();
 
-	    //changes to WYSIWYG mode
+        //fire initialize event
+        this.fireEvent('initialize');
+		
+        //register blur and focus event
+        this.instance.on('blur', $.proxy(function() { this.fireEvent('blur'); }, this));
+        this.instance.on('focus', $.proxy(function() { this.fireEvent('focus'); }, this));
+
+        //changes to WYSIWYG mode
     	this.instance.on('contentDom', $.proxy(function() {
     		this.fireEvent('wysiwygMode');
       	}, this));
  
     	//changes to source mode
     	this.instance.on('mode', $.proxy(function(event) {
-			if (this.instance.mode != 'source') {
-				return;    	
-			}
-			this.fireEvent('sourceMode');
-    	}, this));
+                if (this.instance.mode != 'source') {
+                        return;    	
+                }
+                this.fireEvent('sourceMode');
+            }, this));
 	},
 
 	/**
@@ -231,24 +236,30 @@ PrimeFacesExt.widget.CKEditor = PrimeFaces.widget.DeferredWidget.extend({
 	 * @private
 	 */
 	fireEvent : function(eventName) {
-		if (this.cfg.behaviors) {
-			var callback = this.cfg.behaviors[eventName];
-		    if (callback) {
-		    	var options = {
-		    			params: []
-		    	};
-	
-		    	callback.call(this, options);
-		    }
-		}
+            if (this.cfg.behaviors) {
+                var callback = this.cfg.behaviors[eventName];
+                if (callback) {
+                    var options = {
+                                    params: []
+                    };
+
+                    callback.call(this, options);
+                }
+            }
 	},
 
 	/**
 	 * Destroys the CKEditor instance.
 	 */
 	destroy : function() {
-	    if (this.instance) {
-	        this.instance.destroy(true);
+            if (this.instance) {
+                try {
+                    this.instance.destroy(true);
+                } catch (err) {
+                    if (window.console && console.log) {
+                        console.log('CKEditor throwed a error while destroying the old instance: ' + err);
+                    }
+                }
 	        this.instance = null;
 	    }
 	
