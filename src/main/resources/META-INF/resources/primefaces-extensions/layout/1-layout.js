ef8d5c8ee450b923c253e003b866814b579a8318
melloware <mellowaredev@gmail.com>
2016-12-06 08:12:26 -0500

    Added resizeAll() JS to Layout widget to allow trigger a full resize.

--- src/main/resources/META-INF/resources/primefaces-extensions/layout/1-layout.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/layout/1-layout.js
@@ -1,15 +1,16 @@
 /**
  * PrimeFaces Extensions Layout Widget.
- *
+ * 
  * @author Oleg Varaksin
  */
 PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
     /**
      * Initializes the widget.
-     *
-     * @param {object} cfg The widget configuration.
+     * 
+     * @param {object}
+     *        cfg The widget configuration.
      */
-    init:function (cfg) {
+    init : function(cfg) {
         this._super(cfg);
         this.cfg = cfg;
         this.id = cfg.id;
@@ -20,10 +21,10 @@ PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
 
         if (cfg.clientState) {
             this.cfg.options.stateManagement = {
-                enabled:true,
-                autoSave:true,
-                cookie:{
-                    name:encodeURIComponent('pfext.layout.' + this.id)
+                enabled : true,
+                autoSave : true,
+                cookie : {
+                    name : encodeURIComponent('pfext.layout.' + this.id)
                 }
             };
         } else if (cfg.serverState) {
@@ -33,7 +34,7 @@ PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
         this.renderDeferred();
     },
 
-    _render:function () {
+    _render : function() {
         // create layout
         this.layout = this.jq.layout(this.cfg.options);
 
@@ -45,83 +46,88 @@ PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
         this.bindEvents(this.jq);
     },
 
-    bindEvents:function(parent) {
+    bindEvents : function(parent) {
         var $this = this;
 
         // bind events
-        parent.find(".ui-layout-pane")
-            .on("layoutpaneonopen",function () {
-                var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['open'] : null;
-                if (behavior) {
-                    var combinedPosition = $(this).data('combinedposition');
-                    var options = {
-                        params:[
-                            {name:$this.id + '_pane', value:combinedPosition}
-                        ]
-                    };
-
-                    behavior.call($this, options);
-                }
-
-                if ($this.cfg.serverState) {
-                    $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
-                }
-            }).on("layoutpaneonclose",function () {
-                var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['close'] : null;
-                if (behavior) {
-                    var combinedPosition = $(this).data('combinedposition');
-                    var options = {
-                        params:[
-                            {name:$this.id + '_pane', value:combinedPosition}
-                        ]
-                    };
-
-                    behavior.call($this, options);
-                }
-
-                if ($this.cfg.serverState) {
-                    $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
-                }
-            }).on("layoutpaneonresize", function () {
-                var layoutPane = $(this).data("layoutPane");
-
-                if (!layoutPane.state.isClosed && !layoutPane.state.isHidden) {
-                    var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['resize'] : null;
-                    if (behavior) {
-                        var combinedPosition = $(this).data('combinedposition');
-                        var options = {
-                            params:[
-                                {name:$this.id + '_pane', value:combinedPosition},
-                                {name:$this.id + '_width', value:layoutPane.state.innerWidth},
-                                {name:$this.id + '_height', value:layoutPane.state.innerHeight}
-                            ]
-                        };
-
-                        behavior.call($this, options);
-                    }
-
-                    if ($this.cfg.serverState) {
-                        $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
-                    }
-                }
-            });
-    },
-
-    toggle:function (pane) {
-        this.jq.find(".ui-layout-pane").
-            each(function() {
+        parent.find(".ui-layout-pane").on("layoutpaneonopen", function() {
+            var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['open'] : null;
+            if (behavior) {
                 var combinedPosition = $(this).data('combinedposition');
-                if (combinedPosition && combinedPosition === pane) {
-                    $(this).trigger("layoutpanetoggle");
-                    return false;
+                var options = {
+                    params : [ {
+                        name : $this.id + '_pane',
+                        value : combinedPosition
+                    } ]
+                };
+
+                behavior.call($this, options);
+            }
+
+            if ($this.cfg.serverState) {
+                $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
+            }
+        }).on("layoutpaneonclose", function() {
+            var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['close'] : null;
+            if (behavior) {
+                var combinedPosition = $(this).data('combinedposition');
+                var options = {
+                    params : [ {
+                        name : $this.id + '_pane',
+                        value : combinedPosition
+                    } ]
+                };
+
+                behavior.call($this, options);
+            }
+
+            if ($this.cfg.serverState) {
+                $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
+            }
+        }).on("layoutpaneonresize", function() {
+            var layoutPane = $(this).data("layoutPane");
+
+            if (!layoutPane.state.isClosed && !layoutPane.state.isHidden) {
+                var behavior = $this.cfg.behaviors ? $this.cfg.behaviors['resize'] : null;
+                if (behavior) {
+                    var combinedPosition = $(this).data('combinedposition');
+                    var options = {
+                        params : [ {
+                            name : $this.id + '_pane',
+                            value : combinedPosition
+                        }, {
+                            name : $this.id + '_width',
+                            value : layoutPane.state.innerWidth
+                        }, {
+                            name : $this.id + '_height',
+                            value : layoutPane.state.innerHeight
+                        } ]
+                    };
+
+                    behavior.call($this, options);
                 }
-            });
+
+                if ($this.cfg.serverState) {
+                    $this.stateHiddenField.val($this.layout.encodeJSON($this.layout.readState()));
+                }
+            }
+        });
     },
 
-    close:function (pane) {
+    toggle : function(pane) {
+        this.jq.find(".ui-layout-pane").each(function() {
+            var combinedPosition = $(this).data('combinedposition');
+            if (combinedPosition && combinedPosition === pane) {
+                $(this).trigger("layoutpanetoggle");
+                return false;
+            }
+        });
+    },
+
+    close : function(pane) {
         var panes = this.jq.find(".ui-layout-pane");
         var length = panes.length;
-        for (var i=0; i < length; i++) {
+        for (var i = 0; i < length; i++) {
             var combinedPosition = $(panes[i]).data('combinedposition');
             if (combinedPosition && combinedPosition === pane) {
                 $(panes[i]).trigger("layoutpaneclose");
@@ -130,10 +136,10 @@ PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
         }
     },
 
-    open:function (pane) {
+    open : function(pane) {
         var panes = this.jq.find(".ui-layout-pane");
         var length = panes.length;
-        for (var i=0; i < length; i++) {
+        for (var i = 0; i < length; i++) {
             var combinedPosition = $(panes[i]).data('combinedposition');
             if (combinedPosition && combinedPosition === pane) {
                 $(panes[i]).trigger("layoutpaneopen");
@@ -142,54 +148,37 @@ PrimeFaces.widget.ExtLayout = PrimeFaces.widget.DeferredWidget.extend({
         }
     },
 
-    sizePane:function (pane, size) {
+    sizePane : function(pane, size) {
         var panes = this.jq.find(".ui-layout-pane");
         var length = panes.length;
-        for (var i=0; i < length; i++) {
+        for (var i = 0; i < length; i++) {
             var combinedPosition = $(panes[i]).data('combinedposition');
             if (combinedPosition && combinedPosition === pane) {
-                $(panes[i]).trigger("layoutpanesize", [size]);
+                $(panes[i]).trigger("layoutpanesize", [ size ]);
                 break;
             }
         }
     },
 
-    sizeContent:function (pane) {
+    sizeContent : function(pane) {
         var panes = this.jq.find(".ui-layout-pane");
         var length = panes.length;
-        for (var i=0; i < length; i++) {
+        for (var i = 0; i < length; i++) {
             var combinedPosition = $(panes[i]).data('combinedposition');
             if (combinedPosition && combinedPosition === pane) {
                 $(panes[i]).trigger("layoutpanesizecontent");
                 break;
             }
         }
+    },
+
+    /**
+     * Automatically resize the whole layout.
+     */
+    resizeAll : function() {
+        if (this.layout) {
+            this.layout.resizeAll();
+        }
     }
 
-    /*
-    update:function (pane, options) {
-        var $this = this;
-
-        this.jq.find(".ui-layout-pane").
-            each(function() {
-                var $this = $(this);
-                var combinedPosition = $this.data('combinedposition');
-                if (combinedPosition && combinedPosition === pane) {
-
-                    // update child options
-                    var $layoutContainer = $this.closest(".ui-layout-container");
-                    if ($layoutContainer.length) {
-                        var innerLayout = $layoutContainer.data("layout");
-
-                        innerLayout.destroy();
-                        $layoutContainer.layout(options != null ? options : {});
-
-                        // bind "open", "close" and "resize" events
-                        $this.bindEvents($this);
-                    }
-
-                    return false;
-                }
-            });
-    }*/
 });
