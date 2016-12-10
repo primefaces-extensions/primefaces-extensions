105bb46a5ef166c3516fe72351358afbd5566e39
melloware <mellowaredev@gmail.com>
2016-12-10 09:58:40 -0500

    Fix #429 updated Layout component JS to 1.4.4 and switched Renderer to use WidgetBuilder.

--- src/main/java/org/primefaces/extensions/component/layout/LayoutRenderer.java
+++ src/main/java/org/primefaces/extensions/component/layout/LayoutRenderer.java
@@ -26,149 +26,134 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
 import org.apache.commons.lang3.StringUtils;
-
 import org.primefaces.extensions.model.layout.LayoutOptions;
 import org.primefaces.extensions.util.FastStringWriter;
 import org.primefaces.renderkit.CoreRenderer;
 import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.WidgetBuilder;
 
 /**
  * Renderer for the {@link Layout} component.
  *
- * @author  Oleg Varaksin / last modified by $Author$
+ * @author Oleg Varaksin / last modified by $Author$
  * @version $Revision$
- * @since   0.2
+ * @since 0.2
  */
 public class LayoutRenderer extends CoreRenderer {
 
-	@Override
-	public void decode(FacesContext fc, UIComponent component) {
-		decodeBehaviors(fc, component);
-	}
+   @Override
+   public void decode(final FacesContext fc, final UIComponent component) {
+      decodeBehaviors(fc, component);
+   }
 
-	@Override
-	public void encodeBegin(FacesContext fc, UIComponent component) throws IOException {
-		ResponseWriter writer = fc.getResponseWriter();
-		Layout layout = (Layout) component;
+   @Override
+   public void encodeBegin(final FacesContext fc, final UIComponent component) throws IOException {
+      ResponseWriter writer = fc.getResponseWriter();
+      final Layout layout = (Layout) component;
 
-		boolean buildOptions = layout.getOptions() == null;
-		layout.setBuildOptions(buildOptions);
+      final boolean buildOptions = layout.getOptions() == null;
+      layout.setBuildOptions(buildOptions);
 
-		if (buildOptions) {
-			FastStringWriter fsw = new FastStringWriter();
-			layout.setOriginalWriter(writer);
-			layout.setFastStringWriter(fsw);
-			fc.setResponseWriter(writer.cloneWithWriter(fsw));
-			writer = fc.getResponseWriter();
-		} else {
-			encodeScript(fc, layout);
-		}
+      if (buildOptions) {
+         final FastStringWriter fsw = new FastStringWriter();
+         layout.setOriginalWriter(writer);
+         layout.setFastStringWriter(fsw);
+         fc.setResponseWriter(writer.cloneWithWriter(fsw));
+         writer = fc.getResponseWriter();
+      } else {
+         encodeScript(fc, layout);
+      }
 
-		if (!layout.isFullPage()) {
-			writer.startElement("div", layout);
-			writer.writeAttribute("id", layout.getClientId(fc), "id");
+      if (!layout.isFullPage()) {
+         writer.startElement("div", layout);
+         writer.writeAttribute("id", layout.getClientId(fc), "id");
 
-			if (layout.getStyle() != null) {
-				writer.writeAttribute("style", layout.getStyle(), "style");
-			}
+         if (layout.getStyle() != null) {
+            writer.writeAttribute("style", layout.getStyle(), "style");
+         }
 
-			if (layout.getStyleClass() != null) {
-				writer.writeAttribute("class", layout.getStyleClass(), "styleClass");
-			}
-		}
-	}
+         if (layout.getStyleClass() != null) {
+            writer.writeAttribute("class", layout.getStyleClass(), "styleClass");
+         }
+      }
+   }
 
-	@Override
-	public void encodeEnd(FacesContext fc, UIComponent component) throws IOException {
-		ResponseWriter writer = fc.getResponseWriter();
-		Layout layout = (Layout) component;
+   @Override
+   public void encodeEnd(final FacesContext fc, final UIComponent component) throws IOException {
+      final ResponseWriter writer = fc.getResponseWriter();
+      final Layout layout = (Layout) component;
 
-		if (!layout.isFullPage()) {
-			if (!layout.isStateCookie()) {
-				// render hidden field for server-side state saving
-				String clientId = layout.getClientId(fc);
-				writer.startElement("input", null);
-				writer.writeAttribute("type", "hidden", null);
-				writer.writeAttribute("id", clientId + "_state", null);
-				writer.writeAttribute("name", clientId + "_state", null);
-				writer.writeAttribute("autocomplete", "off", null);
-				writer.endElement("input");
-			}
+      if (!layout.isFullPage()) {
+         if (!layout.isStateCookie()) {
+            // render hidden field for server-side state saving
+            final String clientId = layout.getClientId(fc);
+            writer.startElement("input", null);
+            writer.writeAttribute("type", "hidden", null);
+            writer.writeAttribute("id", clientId + "_state", null);
+            writer.writeAttribute("name", clientId + "_state", null);
+            writer.writeAttribute("autocomplete", "off", null);
+            writer.endElement("input");
+         }
 
-			writer.endElement("div");
-		}
+         writer.endElement("div");
+      }
 
-		if (layout.isBuildOptions()) {
-			fc.setResponseWriter(layout.getOriginalWriter());
-			encodeScript(fc, layout);
-			fc.getResponseWriter().write(layout.getFastStringWriter().toString());
-			layout.removeOptions();
-			layout.setOriginalWriter(null);
-			layout.setFastStringWriter(null);
-		}
+      if (layout.isBuildOptions()) {
+         fc.setResponseWriter(layout.getOriginalWriter());
+         encodeScript(fc, layout);
+         fc.getResponseWriter().write(layout.getFastStringWriter().toString());
+         layout.removeOptions();
+         layout.setOriginalWriter(null);
+         layout.setFastStringWriter(null);
+      }
 
-		layout.setBuildOptions(false);
-	}
+      layout.setBuildOptions(false);
+   }
 
-	@Override
-	public boolean getRendersChildren() {
-		return false;
-	}
+   @Override
+   public boolean getRendersChildren() {
+      return false;
+   }
 
-	protected void encodeScript(FacesContext fc, Layout layout) throws IOException {
-		ResponseWriter writer = fc.getResponseWriter();
-		String clientId = layout.getClientId(fc);
-        String widgetVar = layout.resolveWidgetVar();
+   protected void encodeScript(final FacesContext fc, final Layout layout) throws IOException {
+      final ResponseWriter writer = fc.getResponseWriter();
+      final String clientId = layout.getClientId(fc);
+      final WidgetBuilder wb = getWidgetBuilder(fc);
+      wb.initWithDomReady("ExtLayout", layout.resolveWidgetVar(), clientId);
+      wb.attr("clientState", layout.isStateCookie());
+      if (layout.isFullPage()) {
+         wb.attr("forTarget", "body");
+      } else {
+         wb.attr("forTarget", ComponentUtils.escapeJQueryId(clientId));
+      }
 
-		startScript(writer, clientId);
-		writer.write("$(function() {");
-		writer.write("PrimeFaces.cw('ExtLayout', '" + widgetVar + "',{");
-		writer.write("id:'" + clientId + "'");
-        writer.write(",widgetVar:'" + widgetVar + "'");
+      final ValueExpression stateVE = layout.getValueExpression(Layout.PropertyKeys.state.toString());
+      if (stateVE != null && !layout.isFullPage() && !layout.isStateCookie()) {
+         wb.attr("serverState", true);
 
-		if (layout.isFullPage()) {
-			writer.write(",forTarget:'body'");
-		} else {
-			writer.write(",forTarget:'" + ComponentUtils.escapeJQueryId(clientId) + "'");
-		}
+         final String state = layout.getState();
+         if (StringUtils.isNotBlank(state)) {
+            wb.attr("state", state);
+         } else {
+            wb.nativeAttr("state", "{}");
+         }
+      } else {
+         wb.attr("serverState", false);
+      }
 
-		boolean stateCookie = layout.isStateCookie();
-		writer.write(",clientState:");
-		if (stateCookie) {
-			writer.write("true");
-		} else {
-			writer.write("false");
-		}
+      final Object layoutOptions = layout.getOptions();
+      if (layoutOptions instanceof LayoutOptions) {
+         wb.append(",options:" + ((LayoutOptions) layoutOptions).toJson());
+      } else if (layoutOptions instanceof String) {
+         // already serialized as JSON string
+         wb.append(",options:" + layoutOptions.toString());
+      } else {
+         wb.append(",options:{}");
+      }
 
-		writer.write(",serverState:");
+      encodeClientBehaviors(fc, layout);
 
-		ValueExpression stateVE = layout.getValueExpression(Layout.PropertyKeys.state.toString());
-		if (stateVE != null && !layout.isFullPage() && !stateCookie) {
-			writer.write("true");
-
-			String state = layout.getState();
-			if (StringUtils.isNotBlank(state)) {
-				writer.write(",state:" + state);
-			} else {
-				writer.write(",state:{}");
-			}
-		} else {
-			writer.write("false");
-		}
-
-		Object layoutOptions = layout.getOptions();
-		if (layoutOptions instanceof LayoutOptions) {
-			writer.write(",options:" + ((LayoutOptions) layoutOptions).toJson());
-		} else if (layoutOptions instanceof String) {
-			// already serialized as JSON string
-			writer.write(",options:" + layoutOptions.toString());
-		} else {
-			writer.write(",options:{}");
-		}
-
-		encodeClientBehaviors(fc, layout);
-
-		writer.write("},true);});");
-		endScript(writer);
-	}
+      wb.append("},true);});");
+      endScript(writer);
+   }
 }
