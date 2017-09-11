faaf7a29dc966051cbd82ac84ae17fb6d4fb62e5
melloware <mellowaredev@gmail.com>
2017-09-11 08:00:46 -0400

    Fix #485: Library updates.

--- src/test/java/org/primefaces/extensions/converter/JsonConverterTest.java
+++ src/test/java/org/primefaces/extensions/converter/JsonConverterTest.java
@@ -31,7 +31,7 @@ import java.util.Map;
 
 import javax.faces.convert.ConverterException;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Before;
 import org.junit.Test;
