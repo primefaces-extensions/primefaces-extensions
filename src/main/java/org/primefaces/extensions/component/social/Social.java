0b60a43f46bb8205a45420e017ab515761ae4466
melloware <mellowaredev@gmail.com>
2017-10-31 13:01:06 -0400

    Fix #500: New component Social media.

---
+++ src/main/java/org/primefaces/extensions/component/social/Social.java
@@ -0,0 +1,247 @@
+/**
+ * Copyright 2011-2017 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.component.social;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponentBase;
+import javax.faces.component.behavior.ClientBehaviorHolder;
+
+import org.primefaces.component.api.Widget;
+import org.primefaces.util.ComponentUtils;
+
+/**
+ * <code>Social</code> component.
+ *
+ * @author Melloware mellowaredev@gmail.com
+ * @since 6.2
+ */
+@ResourceDependencies({
+            @ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+            @ResourceDependency(library = "primefaces-extensions", name = "social/social.css"),
+            @ResourceDependency(library = "primefaces-extensions", name = "social/social.js")
+})
+public class Social extends UIComponentBase implements ClientBehaviorHolder, Widget {
+
+    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.Social";
+    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
+    private static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.SocialRenderer";
+
+    protected enum PropertyKeys {
+
+        // @formatter:off
+        widgetVar,
+        url,
+        text,
+        showLabel,
+        showCount,
+        shares,
+        shareIn,
+        theme,
+        emailTo,
+        twitterUsername,
+        twitterHashtags,
+        pinterestMedia,
+        style,
+        styleClass,
+        onclick,
+        onmouseenter,
+        onmouseleave;
+        // @formatter:on
+
+        private String toString;
+
+        PropertyKeys(final String toString) {
+            this.toString = toString;
+        }
+
+        PropertyKeys() {
+        }
+
+        public String getToString() {
+            return toString;
+        }
+
+        public void setToString(String toString) {
+            this.toString = toString;
+        }
+
+        @Override
+        public String toString() {
+            return toString != null ? toString : super.toString();
+        }
+    }
+
+    /**
+     * Default constructor
+     */
+    public Social() {
+        setRendererType(DEFAULT_RENDERER);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String resolveWidgetVar() {
+        return ComponentUtils.resolveWidgetVar(getFacesContext(), this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public String getWidgetVar() {
+        return (String) getStateHelper().eval(PropertyKeys.widgetVar, null);
+    }
+
+    public void setWidgetVar(final String _widgetVar) {
+        getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
+    }
+
+    public String getShares() {
+        return (String) getStateHelper().eval(PropertyKeys.shares, null);
+    }
+
+    public void setShares(final String _text) {
+        getStateHelper().put(PropertyKeys.shares, _text);
+    }
+
+    public String getUrl() {
+        return (String) getStateHelper().eval(PropertyKeys.url, null);
+    }
+
+    public void setUrl(final String _url) {
+        getStateHelper().put(PropertyKeys.url, _url);
+    }
+
+    public String getText() {
+        return (String) getStateHelper().eval(PropertyKeys.text, null);
+    }
+
+    public void setText(final String _text) {
+        getStateHelper().put(PropertyKeys.text, _text);
+    }
+
+    public String getShareIn() {
+        return (String) getStateHelper().eval(PropertyKeys.shareIn, "blank");
+    }
+
+    public void setShareIn(final String _shareIn) {
+        getStateHelper().put(PropertyKeys.shareIn, _shareIn);
+    }
+
+    public String getTheme() {
+        return (String) getStateHelper().eval(PropertyKeys.theme, "classic");
+    }
+
+    public void setTheme(final String _theme) {
+        getStateHelper().put(PropertyKeys.theme, _theme);
+    }
+
+    public void setShowLabel(final boolean _showLabel) {
+        getStateHelper().put(PropertyKeys.showLabel, _showLabel);
+    }
+
+    public boolean isShowLabel() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.showLabel, true);
+    }
+
+    public void setShowCount(final String _showCount) {
+        getStateHelper().put(PropertyKeys.showCount, _showCount);
+    }
+
+    public String getShowCount() {
+        return (String) getStateHelper().eval(PropertyKeys.showCount, "true");
+    }
+
+    public String getStyle() {
+        return (String) getStateHelper().eval(PropertyKeys.style, null);
+    }
+
+    public void setStyle(final String _style) {
+        getStateHelper().put(PropertyKeys.style, _style);
+    }
+
+    public String getStyleClass() {
+        return (String) getStateHelper().eval(PropertyKeys.styleClass, null);
+    }
+
+    public void setStyleClass(final String _styleClass) {
+        getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+    }
+
+    public String getEmailTo() {
+        return (String) getStateHelper().eval(PropertyKeys.emailTo, null);
+    }
+
+    public void setEmailTo(final String _emailTo) {
+        getStateHelper().put(PropertyKeys.emailTo, _emailTo);
+    }
+
+    public String getTwitterUsername() {
+        return (String) getStateHelper().eval(PropertyKeys.twitterUsername, null);
+    }
+
+    public void setTwitterUsername(final String _twitterUsername) {
+        getStateHelper().put(PropertyKeys.twitterUsername, _twitterUsername);
+    }
+
+    public String getTwitterHashtags() {
+        return (String) getStateHelper().eval(PropertyKeys.twitterHashtags, null);
+    }
+
+    public void setTwitterHashtags(final String _twitterHashtags) {
+        getStateHelper().put(PropertyKeys.twitterHashtags, _twitterHashtags);
+    }
+
+    public String getPinterestMedia() {
+        return (String) getStateHelper().eval(PropertyKeys.pinterestMedia, null);
+    }
+
+    public void setPinterestMedia(final String _pinterestMedia) {
+        getStateHelper().put(PropertyKeys.pinterestMedia, _pinterestMedia);
+    }
+
+    public String getOnclick() {
+        return (String) getStateHelper().eval(PropertyKeys.onclick, null);
+    }
+
+    public void setOnclick(final String _onclick) {
+        getStateHelper().put(PropertyKeys.onclick, _onclick);
+    }
+
+    public String getOnmouseenter() {
+        return (String) getStateHelper().eval(PropertyKeys.onmouseenter, null);
+    }
+
+    public void setOnmouseenter(final String _onmouseenter) {
+        getStateHelper().put(PropertyKeys.onmouseenter, _onmouseenter);
+    }
+
+    public String getOnmouseleave() {
+        return (String) getStateHelper().eval(PropertyKeys.onmouseleave, null);
+    }
+
+    public void setOnmouseleave(final String _onmouseleave) {
+        getStateHelper().put(PropertyKeys.onmouseleave, _onmouseleave);
+    }
+
+}
---
+++ src/main/java/org/primefaces/extensions/component/social/SocialRenderer.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright 2011-2017 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.component.social;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.HTML;
+import org.primefaces.util.WidgetBuilder;
+
+/**
+ * Renderer for the {@link Social} component.
+ *
+ * @author Melloware mellowaredev@gmail.com
+ * @since 6.2
+ */
+public class SocialRenderer extends CoreRenderer {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void decode(final FacesContext context, final UIComponent component) {
+        decodeBehaviors(context, component);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
+        final Social social = (Social) component;
+        encodeMarkup(context, social);
+        encodeScript(context, social);
+    }
+
+    /**
+     * Create the HTML markup for the DOM.
+     */
+    private void encodeMarkup(final FacesContext context, final Social social) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final String clientId = social.getClientId(context);
+        final String widgetVar = social.resolveWidgetVar();
+        final String styleClass = social.getTheme() + " " + StringUtils.defaultString(social.getStyleClass());
+
+        writer.startElement("div", social);
+        writer.writeAttribute("id", clientId, "id");
+        writer.writeAttribute(HTML.WIDGET_VAR, widgetVar, null);
+        writer.writeAttribute("class", styleClass, "styleClass");
+        if (social.getStyle() != null) {
+            writer.writeAttribute("style", social.getStyle(), "style");
+        }
+        writer.endElement("div");
+    }
+
+    /**
+     * Create the Javascript.
+     */
+    private void encodeScript(final FacesContext context, final Social social) throws IOException {
+        final WidgetBuilder wb = getWidgetBuilder(context);
+        final String clientId = social.getClientId(context);
+        wb.initWithDomReady("ExtSocial", social.resolveWidgetVar(), clientId);
+        wb.attr("showLabel", social.isShowLabel());
+        wb.attr("shareIn", social.getShareIn());
+        if (StringUtils.isNotBlank(social.getUrl())) {
+            wb.attr("url", social.getUrl());
+        }
+        if (StringUtils.isNotBlank(social.getText())) {
+            wb.attr("text", social.getText());
+        }
+
+        final boolean showCount = BooleanUtils.toBoolean(social.getShowCount());
+        if (showCount) {
+            wb.attr("showCount", showCount);
+        }
+        else {
+            if (social.getShowCount().equalsIgnoreCase("inside")) {
+                wb.attr("showCount", social.getShowCount());
+            }
+            else {
+                wb.attr("showCount", showCount);
+            }
+        }
+
+        // shares array
+        wb.append(",shares: [");
+        String[] shares = StringUtils.split(social.getShares(), ',');
+        for (int i = 0; i < shares.length; i++) {
+            // { share: "pinterest", media: "http://mysite.com" },
+            if (i != 0) {
+                wb.append(",");
+            }
+            String share = StringUtils.lowerCase(shares[i]);
+            wb.append("{");
+            addShareProperty(wb, "share", share);
+            if (share.equalsIgnoreCase("twitter")) {
+                wb.attr("via", social.getTwitterUsername());
+                wb.attr("hashtags", social.getTwitterHashtags());
+            }
+            if (share.equalsIgnoreCase("email")) {
+                wb.attr("to", social.getEmailTo());
+            }
+            if (share.equalsIgnoreCase("pinterest")) {
+                wb.attr("media", social.getPinterestMedia());
+            }
+            wb.append("}");
+        }
+        wb.append("]");
+
+        // javascript
+        wb.append(",on: {");
+        if (social.getOnclick() != null) {
+            addCallback(wb, "click", "function(e)", social.getOnclick());
+        }
+        if (social.getOnmouseenter() != null) {
+            addCallback(wb, "mouseenter", "function(e)", social.getOnmouseenter());
+        }
+        if (social.getOnmouseleave() != null) {
+            addCallback(wb, "mouseleave", "function(e)", social.getOnmouseleave());
+        }
+        wb.append("}");
+
+        encodeClientBehaviors(context, social);
+
+        wb.finish();
+    }
+
+    private void addShareProperty(WidgetBuilder wb, String property, String value) throws IOException {
+        if (value != null) {
+            wb.append(property);
+            wb.append(":\"");
+            wb.append(ComponentUtils.escapeEcmaScriptText(value));
+            wb.append("\"");
+        }
+    }
+
+    public void addCallback(WidgetBuilder wb, String name, String signature, String callback) throws IOException {
+        if (callback != null) {
+            wb.append(name);
+            wb.append(":");
+            wb.append(signature);
+            wb.append("{");
+            wb.append(callback);
+            wb.append("},");
+        }
+    }
+
+}
