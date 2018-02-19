f44e134e72add32f389c6376ac713396a01dd15f
melloware <mellowaredev@gmail.com>
2018-02-19 11:08:44 -0500

    License to 2018.

--- src/main/java/org/primefaces/extensions/component/masterdetail/MasterDetailLevelTagHandler.java
+++ src/main/java/org/primefaces/extensions/component/masterdetail/MasterDetailLevelTagHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2017 PrimeFaces Extensions
+ * Copyright 2011-2018 PrimeFaces Extensions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
