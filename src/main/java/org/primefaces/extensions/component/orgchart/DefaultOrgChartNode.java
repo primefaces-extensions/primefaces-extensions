f133653b81bfec04ffe919c6f7825ab5e4da797a
jxmai <jxmai@users.noreply.github.com>
2018-09-03 11:16:44 -0500

    implement default toString hashcode and equals based on id name, and nodeData

--- src/main/java/org/primefaces/extensions/component/orgchart/DefaultOrgChartNode.java
+++ src/main/java/org/primefaces/extensions/component/orgchart/DefaultOrgChartNode.java
@@ -67,6 +67,60 @@ public class DefaultOrgChartNode implements OrgChartNode {
         this.title = title;
         this.nodeData = nodeData;
     }
+    
+    @Override
+    public String toString() {
+        return "DefaultOrgChartNode [id=" + id + ", name=" + name + ", nodeData=" + nodeData + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((nodeData == null) ? 0 : nodeData.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DefaultOrgChartNode other = (DefaultOrgChartNode) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } 
+        else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } 
+        else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (nodeData == null) {
+            if (other.nodeData != null) {
+                return false;
+            }
+        } 
+        else if (!nodeData.equals(other.nodeData)) {
+            return false;
+        }
+        return true;
+    }
 
     public String getId() {
         return id;
