4cecd92bcc011e30614bdd38f5b9f8f44966aec0
melloware <mellowaredev@gmail.com>
2019-09-20 07:59:13 -0400

    Fix #721: Added Legend component.

---
+++ src/main/java/org/primefaces/extensions/component/legend/Legend.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright 2011-2019 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.component.legend;
+
+import java.util.Map;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponentBase;
+
+import org.primefaces.component.api.Widget;
+import org.primefaces.util.ComponentUtils;
+
+/**
+ * <code>Legend</code> component.
+ *
+ * @author Melloware mellowaredev@gmail.com
+ * @since 7.1
+ */
+@ResourceDependencies({
+            @ResourceDependency(library = "primefaces", name = "components.css"),
+            @ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+            @ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js"),
+            @ResourceDependency(library = "primefaces", name = "core.js"),
+            @ResourceDependency(library = "primefaces-extensions", name = "legend/legend.css"),
+            @ResourceDependency(library = "primefaces-extensions", name = "legend/legend.js")
+})
+public class Legend extends UIComponentBase implements Widget {
+
+    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.Legend";
+    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
+    public static final String STYLE_CLASS_VERTICAL = "ui-legend-vertical ";
+    public static final String STYLE_CLASS_HORIZONTAL = "ui-legend-horizontal ";
+    public static final String SCALE_STYLE = "ui-legend-scale";
+    public static final String LABELS_STYLE = "ui-legend-labels";
+    public static final String TITLE_STYLE = "ui-legend-title";
+    public static final String FOOTER_STYLE = "ui-legend-footer";
+
+    private static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.LegendRenderer";
+
+    protected enum PropertyKeys {
+
+        // @formatter:off
+        widgetVar,
+        style,
+        styleClass,
+        title,
+        footer,
+        values,
+        layout;
+        // @formatter:on
+
+        private String toString;
+
+        PropertyKeys(final String toString) {
+            this.toString = toString;
+        }
+
+        PropertyKeys() {
+        }
+
+        @Override
+        public String toString() {
+            return toString != null ? toString : super.toString();
+        }
+    }
+
+    /**
+     * Default constructor
+     */
+    public Legend() {
+        setRendererType(DEFAULT_RENDERER);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String resolveWidgetVar() {
+        return ComponentUtils.resolveWidgetVar(getFacesContext(), this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public String getLayout() {
+        return (String) getStateHelper().eval(PropertyKeys.layout, "vertical");
+    }
+
+    public void setLayout(final String layout) {
+        getStateHelper().put(PropertyKeys.layout, layout);
+    }
+
+    public String getTitle() {
+        return (String) getStateHelper().eval(PropertyKeys.title, null);
+    }
+
+    public void setTitle(final String title) {
+        getStateHelper().put(PropertyKeys.title, title);
+    }
+
+    public String getFooter() {
+        return (String) getStateHelper().eval(PropertyKeys.footer, null);
+    }
+
+    public void setFooter(final String footer) {
+        getStateHelper().put(PropertyKeys.footer, footer);
+    }
+
+    public String getStyle() {
+        return (String) getStateHelper().eval(PropertyKeys.style, null);
+    }
+
+    public void setStyle(final String _style) {
+        getStateHelper().put(PropertyKeys.style, _style);
+    }
+
+    public String getStyleClass() {
+        return (String) getStateHelper().eval(PropertyKeys.styleClass, null);
+    }
+
+    public void setStyleClass(final String _styleClass) {
+        getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+    }
+
+    public Map<String, String> getValues() {
+        return (Map<String, String>) getStateHelper().eval(PropertyKeys.values, null);
+    }
+
+    public void setValues(final Map<String, String> map) {
+        getStateHelper().put(PropertyKeys.values, map);
+    }
+
+}
---
+++ src/main/java/org/primefaces/extensions/component/legend/LegendRenderer.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright 2011-2019 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.component.legend;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.HTML;
+import org.primefaces.util.WidgetBuilder;
+
+/**
+ * Renderer for the {@link Legend} component.
+ *
+ * @author Melloware mellowaredev@gmail.com
+ * @since 7.1
+ */
+public class LegendRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
+        final Legend legend = (Legend) component;
+        encodeMarkup(context, legend);
+        encodeScript(context, legend);
+    }
+
+    private void encodeMarkup(FacesContext context, Legend legend) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final String clientId = legend.getClientId(context);
+        final String widgetVar = legend.resolveWidgetVar();
+        final String styleClass = legend.getLayout().equalsIgnoreCase("vertical") ? Legend.STYLE_CLASS_VERTICAL
+                    : Legend.STYLE_CLASS_HORIZONTAL + StringUtils.defaultString(legend.getStyleClass());
+
+        writer.startElement("div", legend);
+        writer.writeAttribute("id", clientId, "id");
+        writer.writeAttribute(HTML.WIDGET_VAR, widgetVar, null);
+        writer.writeAttribute("class", styleClass, "styleClass");
+        if (legend.getStyle() != null) {
+            writer.writeAttribute("style", legend.getStyle(), "style");
+        }
+
+        // title
+        encodeTitle(context, legend);
+
+        // items
+        encodeItems(context, legend);
+
+        // footer
+        encodeFooter(context, legend);
+
+        writer.endElement("div");
+    }
+
+    private void encodeItems(FacesContext context, Legend legend) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        // scales
+        writer.startElement("div", null);
+        writer.writeAttribute("class", Legend.SCALE_STYLE, null);
+        writer.startElement("ul", null);
+        writer.writeAttribute("class", Legend.LABELS_STYLE, null);
+
+        // Key=text, Value=Color
+        final Map<String, String> values = legend.getValues();
+        for (final Entry<String, String> item : values.entrySet()) {
+            writer.startElement("li", null);
+            writer.startElement("span", null);
+            writer.writeAttribute("style", "background:" + item.getValue(), null);
+            writer.endElement("span");
+            writer.writeText(item.getKey(), null);
+            writer.endElement("li");
+        }
+        writer.endElement("ul");
+        writer.endElement("div");
+    }
+
+    private void encodeTitle(FacesContext context, Legend legend) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final UIComponent facet = legend.getFacet("title");
+        final String title = legend.getTitle();
+        writer.startElement("div", null);
+        writer.writeAttribute("class", Legend.TITLE_STYLE, null);
+        if (ComponentUtils.shouldRenderFacet(facet)) {
+            facet.encodeAll(context);
+        }
+        else if (title != null) {
+            writer.writeText(title, null);
+        }
+        else {
+            writer.write("&nbsp;");
+        }
+        writer.endElement("div");
+    }
+
+    private void encodeFooter(FacesContext context, Legend legend) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final UIComponent facet = legend.getFacet("footer");
+        final String footer = legend.getFooter();
+        writer.startElement("div", null);
+        writer.writeAttribute("class", Legend.FOOTER_STYLE, null);
+        if (ComponentUtils.shouldRenderFacet(facet)) {
+            facet.encodeAll(context);
+        }
+        else if (footer != null) {
+            writer.writeText(footer, null);
+        }
+        else {
+            writer.write("&nbsp;");
+        }
+        writer.endElement("div");
+    }
+
+    private void encodeScript(FacesContext context, Legend legend) throws IOException {
+        final WidgetBuilder wb = getWidgetBuilder(context);
+        wb.init("ExtLegend", legend.resolveWidgetVar(), legend.getClientId(context));
+        wb.attr("layout", legend.getLayout());
+        wb.finish();
+    }
+
+}
