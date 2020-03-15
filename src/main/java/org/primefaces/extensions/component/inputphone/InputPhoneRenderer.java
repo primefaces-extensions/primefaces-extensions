a1167dd0d8ffe6640c7b3eeaaa844a1dc7f3679f
jasper <jepsar@gmail.com>
2020-03-15 10:35:59 +0100

    Added check on availability of com.google.i18n.phonenumbers.Phonenumber

--- src/main/java/org/primefaces/extensions/component/inputphone/InputPhoneRenderer.java
+++ src/main/java/org/primefaces/extensions/component/inputphone/InputPhoneRenderer.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import javax.faces.FacesException;
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
@@ -29,7 +28,6 @@ import javax.faces.convert.ConverterException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.primefaces.component.inputtext.InputText;
-import org.primefaces.extensions.util.MessageFactory;
 import org.primefaces.renderkit.InputRenderer;
 import org.primefaces.shaded.json.JSONArray;
 import org.primefaces.util.ComponentUtils;
@@ -38,9 +36,8 @@ import org.primefaces.util.HTML;
 import org.primefaces.util.LangUtils;
 import org.primefaces.util.WidgetBuilder;
 
-import com.google.i18n.phonenumbers.NumberParseException;
-import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import com.google.i18n.phonenumbers.Phonenumber;
+import org.primefaces.extensions.config.PrimeExtensionsEnvironment;
+import org.primefaces.extensions.util.PhoneNumberUtilWrapper;
 
 /**
  * Renderer for the {@link InputPhone} component.
@@ -49,8 +46,6 @@ import com.google.i18n.phonenumbers.Phonenumber;
  * @since 7.0
  */
 public class InputPhoneRenderer extends InputRenderer {
-
-    private static final String MESSAGE_INVALID_VALUE_KEY = "primefaces.extensions.inputphone.INVALID";
 
     @Override
     public void decode(FacesContext context, UIComponent component) {
@@ -102,21 +97,10 @@ public class InputPhoneRenderer extends InputRenderer {
         if (country == null || InputPhone.COUNTRY_AUTO.equals(country)) {
             country = Constants.EMPTY_STRING;
         }
-        try {
-            final PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
-            final Phonenumber.PhoneNumber phoneNumber = phoneNumberUtil.parse(value, country.toUpperCase());
-            if (!phoneNumberUtil.isValidNumber(phoneNumber)) {
-                throw getInvalidValueConverterException();
-            }
-            return value;
+        if (PrimeExtensionsEnvironment.getCurrentInstance(context).isLibphonenumberAvailable()) {
+            PhoneNumberUtilWrapper.validate(value, country.toUpperCase());
         }
-        catch (final NumberParseException e) {
-            throw getInvalidValueConverterException();
-        }
-    }
-
-    protected ConverterException getInvalidValueConverterException() {
-        return new ConverterException(MessageFactory.getMessage(MESSAGE_INVALID_VALUE_KEY, FacesMessage.SEVERITY_ERROR));
+        return value;
     }
 
     protected void encodeMarkup(FacesContext context, InputPhone inputPhone, String valueToRender) throws IOException {
---
+++ src/main/java/org/primefaces/extensions/util/PhoneNumberUtilWrapper.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2011-2019 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.util;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import javax.faces.application.FacesMessage;
+import javax.faces.convert.ConverterException;
+
+/**
+ * @author Jasper de Vries &lt;jepsar@gmail.com&gt;
+ * @since 8.0.1
+ */
+public class PhoneNumberUtilWrapper {
+
+    private static final String MESSAGE_INVALID_VALUE_KEY = "primefaces.extensions.inputphone.INVALID";
+
+    public static void validate(final String number, final String country) {
+        try {
+            final PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+            final Phonenumber.PhoneNumber phoneNumber = phoneNumberUtil.parse(number, country);
+            if (!phoneNumberUtil.isValidNumber(phoneNumber)) {
+                throw getInvalidValueConverterException();
+            }
+        }
+        catch (final NumberParseException e) {
+            throw getInvalidValueConverterException();
+        }
+    }
+
+    private static ConverterException getInvalidValueConverterException() {
+        return new ConverterException(MessageFactory.getMessage(MESSAGE_INVALID_VALUE_KEY, FacesMessage.SEVERITY_ERROR));
+    }
+
+}
