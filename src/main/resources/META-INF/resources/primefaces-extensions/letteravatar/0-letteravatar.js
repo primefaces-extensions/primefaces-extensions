3c9f7ff71c07509078274d1b07ec18cc30fbb071
aripddev <dev@aripd.com>
2020-03-27 17:58:16 +0300

    removed unnecessary variables and lines

--- src/main/resources/META-INF/resources/primefaces-extensions/letteravatar/0-letteravatar.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/letteravatar/0-letteravatar.js
@@ -5,12 +5,8 @@
         name = name || '';
         size = size || 60;
 
-        var colours = [
-            "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",
-            "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
-        ],
-                nameSplit = String(name).toUpperCase().split(' '),
-                initials, charIndex, colourIndex, canvas, context, dataURI;
+        var nameSplit = String(name).toUpperCase().split(' '),
+                initials, canvas, context, dataURI;
 
 
         if (nameSplit.length == 1) {
@@ -23,14 +19,11 @@
             size = (size * w.devicePixelRatio);
         }
 
-        charIndex = (initials == '?' ? 72 : initials.charCodeAt(0)) - 64;
-        colourIndex = charIndex % colours.length;
         canvas = d.createElement('canvas');
         canvas.width = size;
         canvas.height = size;
         context = canvas.getContext("2d");
 
-//        context.fillStyle = colours[colourIndex - 1];
         context.fillStyle = 'hsl(' + LetterAvatar.hue(String(name)) + ', 100%, 50%)';
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.font = Math.round(canvas.width / 2) + "px Arial";
