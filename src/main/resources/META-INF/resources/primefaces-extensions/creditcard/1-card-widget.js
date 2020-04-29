ca45110c43737823e0573caeeae4083ce8b7961c
melloware <mellowaredev@gmail.com>
2020-04-29 19:32:29 -0400

    Fix #791: CreditCard refresh after validation failure.

--- src/main/resources/META-INF/resources/primefaces-extensions/creditcard/1-card-widget.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/creditcard/1-card-widget.js
@@ -1,6 +1,6 @@
 /**
  * PrimeFaces Extensions CreditCard Widget.
- * 
+ *
  * @author Melloware mellowaredev@gmail.com
  * @since 8.0.1
  */
@@ -8,20 +8,40 @@ PrimeFaces.widget.ExtCreditCard = PrimeFaces.widget.BaseWidget.extend({
 
     /**
      * Initializes the widget.
-     * 
+     *
      * @param {object}
      *        cfg The widget configuration.
      */
-    init : function(cfg) {
+    init: function (cfg) {
         this._super(cfg);
         this.id = cfg.id;
         this.cfg = cfg;
         this.cfg.container = '.ui-credit-card';
-        
+
         var closestForm = this.jq.closest('form');
         this.cfg.form = PrimeFaces.escapeClientId(closestForm.attr('id'))
-        
-        this.card = new Card(this.cfg);
+
+        this.card = new Card(this.cfg)
+
+        this.bindListeners();
+    },
+
+    /**
+     * GitHub #791: If any input box values have changed dynamically then
+     * trigger blur/change for Card.js to pick up the changes. Especially
+     * after AJAX postback.
+     */
+    bindListeners: function () {
+        this.addRefreshListener(function () {
+            $(document).ready(function () {
+                const blur = new Event('blur');
+                const change = new Event('change');
+                $('.ui-state-filled').each(function (index) {
+                    this.dispatchEvent(blur);
+                    this.dispatchEvent(change);
+                })
+            });
+        });
     }
 });
 
