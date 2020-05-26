ac83734909e6370f247c4d3c5093669e06f2433e
melloware <mellowaredev@gmail.com>
2020-05-26 12:48:29 -0400

    Fix #799: Timer JS cleanup

--- src/main/resources/META-INF/resources/primefaces-extensions/timer/timer.js
+++ src/main/resources/META-INF/resources/primefaces-extensions/timer/timer.js
@@ -1,114 +1,147 @@
 /**
- * Primefaces Extension Timer Widget
+ * PrimeFaces Extension Timer Widget
  *
  * @author f.strazzullo
  */
 PrimeFaces.widget.ExtTimer = PrimeFaces.widget.BaseWidget.extend({
 
-    init : function(cfg) {
+    /**
+     * @override
+     * @inheritdoc
+     * @param {PrimeFaces.PartialWidgetCfg<TCfg>} cfg
+     */
+    init: function (cfg) {
         this._super(cfg);
         this.cfg = cfg;
         this.forward = cfg.forward;
         this.originalTimeout = cfg.timeout;
         this.currentTimeout = this.forward ? 0 : cfg.timeout;
 
-        if(cfg.autoStart){
+        if (cfg.autoStart) {
             this.start();
         }
 
         this.print();
-
     },
-    currentTimeInSecs: function() {
-      return parseInt((new Date()).getTime() / 1000, 10);
-    },
-    print:function(){
 
+    /**
+     * Calculate the current time in seconds.
+     * @private
+     * @returns {number} the number of seconds in the current time
+     */
+    currentTimeInSecs: function () {
+        return new Date().getTime() / 1000;
+    },
+
+    /**
+     * Prints the current step to the UI.
+     * @private
+     */
+    print: function () {
         var value = this.currentTimeout;
 
-        if(this.cfg.formatFunction){
+        if (this.cfg.formatFunction) {
             value = this.cfg.formatFunction(value);
-        }else if(this.cfg.format){
+        } else if (this.cfg.format) {
 
             var format = this.cfg.format;
 
-            if("percentage" === format){
-                value = ((this.currentTimeout*100)/this.originalTimeout) + "%";
-            }else if("human" === format){
-                value = moment.duration(this.currentTimeout,'seconds').humanize();
-            }else{
-                value = moment.utc(moment.duration(this.currentTimeout,'seconds').asMilliseconds()).format(format);
+            if ("percentage" === format) {
+                value = ((this.currentTimeout * 100) / this.originalTimeout) + "%";
+            } else if ("human" === format) {
+                value = moment.duration(this.currentTimeout, 'seconds').humanize();
+            } else {
+                value = moment.utc(moment.duration(this.currentTimeout, 'seconds').asMilliseconds()).format(format);
             }
         }
 
         this.jq.html(value);
     },
-    doStep: function(){
-        var seconds = this.currentTimeInSecs() - this.prevTime;
-        this.prevTime = this.currentTimeInSecs();
+
+    /**
+     * Perform the increment of the second
+     * @private
+     */
+    doStep: function () {
+        var currentSeconds = this.currentTimeInSecs(),
+            seconds = currentSeconds - this.prevTime;
+        this.prevTime = currentSeconds;
         this.currentTimeout += this.forward ? seconds : (0 - seconds);
         this.print();
-        if(this.cfg.ontimerstep){
+        if (this.cfg.ontimerstep) {
             this.cfg.ontimerstep({
-                current:this.currentTimeout,
-                total:this.originalTimeout
+                current: this.currentTimeout,
+                total: this.originalTimeout
             });
         }
     },
-    start : function() {
 
-        var that = this;
-        var end;
+    /**
+     * Start the timer.
+     */
+    start: function () {
+        var $this = this;
         this.prevTime = this.currentTimeInSecs();
 
-        if(!this.interval){
-            this.interval = setInterval(function(){
-                that.doStep();
+        if (!this.interval) {
+            this.interval = setInterval(function () {
+                $this.doStep();
 
-                end = that.forward ? that.currentTimeout >= that.originalTimeout : that.currentTimeout <= 0;
+                var end = $this.forward ? $this.currentTimeout >= $this.originalTimeout : $this.currentTimeout <= 0;
 
-                if(end){
-                    if(that.cfg.listener){
-                        that.cfg.listener();
+                if (end) {
+                    if ($this.cfg.listener) {
+                        $this.cfg.listener();
                     }
-                    if(that.cfg.ontimercomplete){
-                        that.cfg.ontimercomplete();
+                    if ($this.cfg.ontimercomplete) {
+                        $this.cfg.ontimercomplete();
                     }
-                    if(that.cfg.singleRun){
-                        clearInterval(that.interval);
-                        this.interval = null;
-                    }else{
-                        that.currentTimeout = that.forward ? 0 : that.originalTimeout;
-                        that.print();
+                    if ($this.cfg.singleRun) {
+                        $this.pause();
+                    } else {
+                        $this.currentTimeout = $this.forward ? 0 : $this.originalTimeout;
+                        $this.print();
                     }
                 }
-            }, that.cfg.interval);
+            }, $this.cfg.interval);
         }
 
     },
-    pause: function(){
-        if(this.interval){
+
+    /**
+     * Pause the timer.
+     */
+    pause: function () {
+        if (this.interval) {
             clearInterval(this.interval);
             this.interval = null;
         }
     },
-    stop: function(silent){
-        if(!silent && this.cfg.listener){
+
+    /**
+     * Stop the timer.
+     *
+     * @param {boolean} silent true if you don't want to fire any AJAX events
+     */
+    stop: function (silent) {
+        if (!silent && this.cfg.listener) {
             this.cfg.listener();
         }
-        if(this.cfg.ontimercomplete){
+        if (this.cfg.ontimercomplete) {
             this.cfg.ontimercomplete();
         }
-        if(this.interval){
-            clearInterval(this.interval);
-            this.interval = null;
-        }
 
+        this.pause();
         this.currentTimeout = this.forward ? 0 : this.originalTimeout;
         this.print();
-
     },
-    restart:function(silent){
+
+    /**
+     * Restart the timer back at its original value.
+     *
+     * @param {boolean} silent true if you don't want to fire any AJAX events
+     */
+    restart: function (silent) {
         this.stop(silent);
         this.start();
     }
