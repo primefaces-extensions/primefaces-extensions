a48e096d3a3590e85543ef30132e00df63d2abcf
jasper <jepsar@gmail.com>
2020-08-23 19:19:40 +0200

    Added start, stop

--- src/main/java/org/primefaces/extensions/component/codescanner/CodeScanner.java
+++ src/main/java/org/primefaces/extensions/component/codescanner/CodeScanner.java
@@ -60,7 +60,8 @@ public class CodeScanner extends UIComponentBase implements Widget, ClientBehavi
         style,
         styleClass,
         width,
-        height
+        height,
+        start
     }
 
     public enum ReaderType {
@@ -134,6 +135,14 @@ public class CodeScanner extends UIComponentBase implements Widget, ClientBehavi
 
     public void setHeight(Integer height) {
         getStateHelper().put(PropertyKeys.height, height);
+    }
+
+    public boolean getStart() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.start, true);
+    }
+
+    public void setStart(boolean start) {
+        getStateHelper().put(PropertyKeys.start, start);
     }
 
     @Override
