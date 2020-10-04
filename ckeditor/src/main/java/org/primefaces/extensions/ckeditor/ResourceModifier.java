00b59d40cc2a78b7242274ffb7e4446b7efe8b18
melloware <mellowaredev@gmail.com>
2020-10-04 10:15:24 -0400

    Spotless cleanup.

--- ckeditor/src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
+++ ckeditor/src/main/java/org/primefaces/extensions/ckeditor/ResourceModifier.java
@@ -26,6 +26,7 @@ import org.apache.commons.io.FileUtils;
 
 /**
  * Steps to update:
+ *
  * <pre>
  * - Delete all files in the src/main/resources/META-INF/resources/primefaces-extensions/ckeditor/ directory
  * - extract new ckeditor files
