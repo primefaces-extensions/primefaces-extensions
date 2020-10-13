48dcb3039dfb4ca7dd6c3520030f7f422c1f3d6f
melloware <mellowaredev@gmail.com>
2020-10-13 08:43:39 -0400

    Sonar add @overide

--- showcase/src/main/java/org/primefaces/extensions/showcase/webapp/CharacterEncodingFilter.java
+++ showcase/src/main/java/org/primefaces/extensions/showcase/webapp/CharacterEncodingFilter.java
@@ -22,12 +22,11 @@ import javax.servlet.*;
 /**
  * Filter that sets the character encoding to be used in parsing the incoming request, either unconditionally or only if the client did not specify a character
  * encoding.
- *
- * @author ova / last modified by $Author$
- * @version $Revision$
  */
 public class CharacterEncodingFilter implements Filter {
-    /** The default character encoding to set for requests that pass through this filter. */
+    /**
+     * The default character encoding to set for requests that pass through this filter.
+     */
     protected String encoding = null;
 
     /**
@@ -35,20 +34,22 @@ public class CharacterEncodingFilter implements Filter {
      */
     protected FilterConfig filterConfig = null;
 
-    /** Should a character encoding specified by the client be ignored? */
+    /**
+     * Should a character encoding specified by the client be ignored?
+     */
     protected boolean ignore = true;
 
-    public void destroy() {
-        this.encoding = null;
-        this.filterConfig = null;
+    @Override public void destroy() {
+        encoding = null;
+        filterConfig = null;
     }
 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    @Override public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
                 throws IOException, ServletException {
         // Conditionally select and set the character encoding to be used
         if (ignore || (request.getCharacterEncoding() == null)) {
-            if (this.encoding != null) {
-                request.setCharacterEncoding(this.encoding);
+            if (encoding != null) {
+                request.setCharacterEncoding(encoding);
             }
         }
 
@@ -56,22 +57,22 @@ public class CharacterEncodingFilter implements Filter {
         chain.doFilter(request, response);
     }
 
-    public void init(FilterConfig filterConfig) throws ServletException {
+    @Override public void init(FilterConfig filterConfig) throws ServletException {
         this.filterConfig = filterConfig;
-        this.encoding = filterConfig.getInitParameter("encoding");
+        encoding = filterConfig.getInitParameter("encoding");
 
         String value = filterConfig.getInitParameter("ignore");
         if (value == null) {
-            this.ignore = true;
+            ignore = true;
         }
         else if (value.equalsIgnoreCase("true")) {
-            this.ignore = true;
+            ignore = true;
         }
         else if (value.equalsIgnoreCase("yes")) {
-            this.ignore = true;
+            ignore = true;
         }
         else {
-            this.ignore = false;
+            ignore = false;
         }
     }
 }
