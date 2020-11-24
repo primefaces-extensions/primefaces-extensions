52dfa9232e8ea681ef814b786f3d4e9f0f0273d0
melloware <mellowaredev@gmail.com>
2020-11-24 08:20:02 -0500

    Fix #235: OrgChart JSON error calling getHiearcharchy

--- core/src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
+++ core/src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
@@ -1,74 +1,74 @@
 /**
  * PrimeFaces Extensions Org Chart Widget.
- * 
+ *
  * @author jxmai
  * @since 6.3
  */
 PrimeFaces.widget.ExtOrgChart = PrimeFaces.widget.BaseWidget.extend({
 
-   /**
-    * Initializes the widget.
-    * 
-    * @param {object}
-    *        cfg The widget configuration.
-    */
-   init : function(cfg) {
-      this._super(cfg);
-      this.id = cfg.id;
-      
-      // user extension to configure plugin
-      var extender = this.cfg.extender
-      if (extender) {
-          if (typeof extender === "function") {
-              extender.call(this);
-          } else {
-              PrimeFaces.error("Extender value is not a javascript function!");
-          }
-      }
+    /**
+     * Initializes the widget.
+     *
+     * @param {object}
+     *        cfg The widget configuration.
+     */
+    init: function (cfg) {
+        this._super(cfg);
+        this.id = cfg.id;
 
-      var opts = $.extend(true, {}, cfg);
-      opts['data'] = JSON.parse(opts['data']);
-      
-      this.jq.orgchart(opts);
+        // user extension to configure plugin
+        var extender = this.cfg.extender;
+        if (extender) {
+            if (typeof extender === "function") {
+                extender.call(this);
+            } else {
+                PrimeFaces.error("Extender value is not a javascript function!");
+            }
+        }
 
-      this._bindEvents();
-   },
+        var opts = $.extend(true, {}, cfg);
+        opts['data'] = JSON.parse(opts['data']);
 
-   _bindEvents : function() {
-      var $this = this;
+        this.orgchart = this.jq.orgchart(opts);
 
-      this.jq.on('click', '.node', function() {
-         var $thisNode = $(this);
+        this._bindEvents();
+    },
 
-         var options = {
-                 params : [ {
-                    name : $this.id + '_nodeId',
-                    value : $thisNode[0].id
-                 }, {
-                    name : $this.id + '_hierarchy',
-                    value : JSON.stringify($this.jq.orgchart('getHierarchy'))
-                 } ]
+    _bindEvents: function () {
+        var $this = this;
 
-              };
-         $this.callBehavior('click', options);
-      });
+        this.jq.on('click', '.node', function () {
+            var $thisNode = $(this);
 
-      this.jq.children('.orgchart').on('nodedropped.orgchart', function(event) {
-          var options = {
-                  params : [ {
-                     name : $this.id + '_draggedNodeId',
-                     value : event.draggedNode['context']['id']
-                  }, {
-                     name : $this.id + '_droppedZoneId',
-                     value : event.dropZone['context']['id']
-                  }, {
-                     name : $this.id + '_hierarchy',
-                     value : JSON.stringify($this.jq.orgchart('getHierarchy'))
-                  } ]
+            var options = {
+                params: [{
+                    name: $this.id + '_nodeId',
+                    value: $thisNode[0].id
+                }, {
+                    name: $this.id + '_hierarchy',
+                    value: JSON.stringify($this.orgchart.getHierarchy())
+                }]
 
-               };
-         $this.callBehavior('drop', options);
+            };
+            $this.callBehavior('click', options);
+        });
 
-      });
-   }
+        this.jq.children('.orgchart').on('nodedropped.orgchart', function (event) {
+            var options = {
+                params: [{
+                    name: $this.id + '_draggedNodeId',
+                    value: event.draggedNode['context']['id']
+                }, {
+                    name: $this.id + '_droppedZoneId',
+                    value: event.dropZone['context']['id']
+                }, {
+                    name: $this.id + '_hierarchy',
+                    value: JSON.stringify($this.orgchart.getHierarchy())
+                }]
+
+            };
+            $this.callBehavior('drop', options);
+
+        });
+    }
 });
