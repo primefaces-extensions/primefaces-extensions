3b2cde328fa0c83cbd7c9cbe0a71e796d99646ff
melloware <mellowaredev@gmail.com>
2021-01-29 10:01:31 -0500

    Fix #293: OmniFaces CombinedResourceHandler convenience classes

---
+++ core/src/main/java/org/primefaces/extensions/application/PrimeFacesResourceProcessor.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2011-2021 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.application;
+
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import org.primefaces.config.PrimeConfiguration;
+import org.primefaces.context.PrimeApplicationContext;
+import org.primefaces.context.PrimeRequestContext;
+
+/**
+ * Creates a custom PhaseListener for RENDER_RESPONSE phase which will during beforePhase() dynamically add those PrimeFaces resources via
+ * UIViewRoot#addComponentResource(). This will run far before those @ResourceDependency annotations are processed. This satisfies PrimeFaces' intent of having
+ * those hardcoded resources to be rendered before of the dependencies of their components.
+ * <p>
+ * Register it as below in faces-config.xml:
+ * </p>
+ * 
+ * <pre>
+ *   &lt;lifecycle&gt;
+ *      &lt;phase-listener&gt;org.primefaces.extensions.application.PrimeFacesResourceProcessor&lt;/phase-listener&gt;
+ *   &lt;/lifecycle&gt;
+ * </pre>
+ *
+ * @see <a href="https://github.com/omnifaces/omnifaces/wiki/Combine-hardcoded-PrimeFaces-resources-using-CombinedResourceHandler">OmniFaces</a>
+ * @since 10.0.0
+ */
+public class PrimeFacesResourceProcessor implements PhaseListener {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+        // NOOP.
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        FacesContext context = event.getFacesContext();
+        PrimeRequestContext requestContext = PrimeRequestContext.getCurrentInstance(context);
+        PrimeApplicationContext applicationContext = requestContext.getApplicationContext();
+        PrimeConfiguration configuration = applicationContext.getConfig();
+
+        String theme;
+        String themeParamValue = configuration.getTheme();
+
+        if (themeParamValue != null) {
+            ELContext elContext = context.getELContext();
+            ExpressionFactory expressionFactory = context.getApplication().getExpressionFactory();
+            ValueExpression ve = expressionFactory.createValueExpression(elContext, themeParamValue, String.class);
+            theme = (String) ve.getValue(elContext);
+        }
+        else {
+            theme = "saga"; // default
+        }
+
+        if (theme != null && !"none".equals(theme)) {
+            encodeCSS(context, "primefaces-" + theme, "theme.css");
+        }
+
+        // Icons
+        encodeCSS(context, "primefaces", "primeicons/primeicons.css");
+        if (applicationContext.getConfig().isFontAwesomeEnabled()) {
+            encodeCSS(context, "primefaces", "fa/font-awesome.css");
+        }
+
+        if (configuration.isClientSideValidationEnabled()) {
+            encodeValidationResources(context, configuration.isBeanValidationEnabled());
+        }
+    }
+
+    protected void encodeValidationResources(FacesContext context, boolean beanValidationEnabled) {
+        encodeJS(context, "validation/validation.js");
+
+        if (beanValidationEnabled) {
+            encodeJS(context, "validation/validation.bv.js");
+        }
+    }
+
+    private void encodeCSS(FacesContext context, String library, String name) {
+        final UIOutput css = new UIOutput();
+        css.setRendererType("javax.faces.resource.Stylesheet");
+        css.getAttributes().put("library", library);
+        css.getAttributes().put("name", name);
+        context.getViewRoot().addComponentResource(context, css, "head");
+    }
+
+    private void encodeJS(FacesContext context, String name) {
+        final UIOutput js = new UIOutput();
+        js.setRendererType("javax.faces.resource.Script");
+        js.getAttributes().put("library", "primefaces");
+        js.getAttributes().put("name", name);
+        context.getViewRoot().addComponentResource(context, js, "head");
+    }
+
+}
---
+++ core/src/main/java/org/primefaces/extensions/application/PrimeFacesScriptProcessor.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2011-2021 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.application;
+
+import java.util.List;
+
+import javax.faces.application.ProjectStage;
+import javax.faces.component.UIOutput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+import javax.faces.lifecycle.ClientWindow;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.primefaces.clientwindow.PrimeClientWindow;
+import org.primefaces.clientwindow.PrimeClientWindowUtils;
+import org.primefaces.config.PrimeConfiguration;
+import org.primefaces.context.PrimeApplicationContext;
+import org.primefaces.context.PrimeRequestContext;
+import org.primefaces.util.LocaleUtils;
+
+/**
+ * Creates a custom SystemEventListener for PostAddToViewEvent on UIViewRoot. This will run after all those @ResourceDependency annotations of PrimeFaces
+ * components have been processed. This is thus an ideal moment to add the PrimeFaces.settings script as a component resource, as intended by PrimeFaces. <br/>
+ * Register it as below in faces-config.xml:
+ * </p>
+ * 
+ * <pre>
+ *     &lt;application&gt;
+ *        &lt;system-event-listener&gt;
+ *            &lt;system-event-listener-class>com.example.PrimeFacesScriptProcessor&lt;/system-event-listener-class&gt;
+ *            &lt;system-event-class>javax.faces.event.PostAddToViewEvent&lt;/system-event-class&gt;
+ *            &lt;source-class>javax.faces.component.UIViewRoot&lt;/source-class&gt;
+ *        &lt;/system-event-listener&gt;
+ *     &lt;/application&gt;
+ * </pre>
+ *
+ * @see <a href="https://github.com/omnifaces/omnifaces/wiki/Combine-hardcoded-PrimeFaces-resources-using-CombinedResourceHandler">OmniFaces</a>
+ * @since 10.0.0
+ */
+public class PrimeFacesScriptProcessor implements SystemEventListener {
+
+    @Override
+    public boolean isListenerForSource(Object source) {
+        return source instanceof UIViewRoot;
+    }
+
+    @Override
+    public void processEvent(SystemEvent event) throws AbortProcessingException {
+        FacesContext context = event.getFacesContext();
+        StringBuilder script = new StringBuilder(4000);
+
+        encodeSettingScripts(context, script);
+        encodeInitScripts(context, script);
+
+        addJS(context, script.toString());
+    }
+
+    protected void encodeSettingScripts(FacesContext context, StringBuilder writer) {
+        PrimeRequestContext requestContext = PrimeRequestContext.getCurrentInstance(context);
+        PrimeApplicationContext applicationContext = requestContext.getApplicationContext();
+        PrimeConfiguration configuration = applicationContext.getConfig();
+
+        ProjectStage projectStage = context.getApplication().getProjectStage();
+
+        writer.append("if(window.PrimeFaces){");
+
+        writer.append("PrimeFaces.settings.locale='").append(LocaleUtils.getCurrentLocale(context)).append("';");
+        writer.append("PrimeFaces.settings.viewId='").append(context.getViewRoot().getViewId()).append("';");
+        writer.append("PrimeFaces.settings.contextPath='").append(context.getExternalContext().getRequestContextPath()).append("';");
+
+        writer.append("PrimeFaces.settings.cookiesSecure=").append(requestContext.isSecure() && configuration.isCookiesSecure()).append(";");
+        if (applicationContext.getConfig().getCookiesSameSite() != null) {
+            writer.append("PrimeFaces.settings.cookiesSameSite='").append(configuration.getCookiesSameSite()).append("';");
+        }
+
+        if (configuration.isClientSideValidationEnabled()) {
+            writer.append("PrimeFaces.settings.validateEmptyFields=").append(configuration.isValidateEmptyFields()).append(";");
+            writer.append("PrimeFaces.settings.considerEmptyStringNull=").append(configuration.isInterpretEmptyStringAsNull()).append(";");
+        }
+
+        if (configuration.isLegacyWidgetNamespace()) {
+            writer.append("PrimeFaces.settings.legacyWidgetNamespace=true;");
+        }
+
+        if (configuration.isEarlyPostParamEvaluation()) {
+            writer.append("PrimeFaces.settings.earlyPostParamEvaluation=true;");
+        }
+
+        if (configuration.isPartialSubmitEnabled()) {
+            writer.append("PrimeFaces.settings.partialSubmit=true;");
+        }
+
+        if (projectStage != ProjectStage.Production) {
+            writer.append("PrimeFaces.settings.projectStage='").append(projectStage.toString()).append("';");
+        }
+
+        if (applicationContext.getEnvironment().isAtLeastJsf22()) {
+            if (context.getExternalContext().getClientWindow() != null) {
+                ClientWindow clientWindow = context.getExternalContext().getClientWindow();
+                if (clientWindow instanceof PrimeClientWindow) {
+
+                    boolean initialRedirect = false;
+
+                    Object cookie = PrimeClientWindowUtils.getInitialRedirectCookie(context, clientWindow.getId());
+                    if (cookie instanceof Cookie) {
+                        Cookie servletCookie = (Cookie) cookie;
+                        initialRedirect = true;
+
+                        // expire/remove cookie
+                        servletCookie.setMaxAge(0);
+                        ((HttpServletResponse) context.getExternalContext().getResponse()).addCookie(servletCookie);
+                    }
+                    writer.append(
+                                String.format("PrimeFaces.clientwindow.init('%s', %s);",
+                                            PrimeClientWindowUtils.secureWindowId(clientWindow.getId()),
+                                            initialRedirect));
+                }
+            }
+        }
+
+        writer.append("}");
+    }
+
+    protected void encodeInitScripts(FacesContext context, StringBuilder writer) {
+        PrimeRequestContext requestContext = PrimeRequestContext.getCurrentInstance(context);
+        List<String> scripts = requestContext.getInitScriptsToExecute();
+
+        if (!scripts.isEmpty()) {
+            boolean moveScriptsToBottom = requestContext.getApplicationContext().getConfig().isMoveScriptsToBottom();
+
+            if (!moveScriptsToBottom) {
+                writer.append("$(function(){");
+            }
+
+            for (String script : scripts) {
+                writer.append(script);
+                writer.append(';');
+            }
+
+            if (!moveScriptsToBottom) {
+                writer.append("});");
+            }
+        }
+    }
+
+    private void addJS(FacesContext context, String script) {
+        final UIOutput js = new UIOutput();
+        js.setRendererType("javax.faces.resource.Script");
+        final UIOutput content = new UIOutput();
+        content.setValue(script);
+        js.getChildren().add(content);
+        context.getViewRoot().addComponentResource(context, js);
+    }
+}
