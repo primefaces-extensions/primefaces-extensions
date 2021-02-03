b62ccaab538a3d6e6115a0b2c6b86068314687cf
jasper <jepsar@gmail.com>
2021-02-03 21:09:11 +0100

    Change theme oncomplete to prevent conflict with PrimePartialResponseWriter resources

--- core/src/main/resources/META-INF/resources/primefaces-extensions/lightswitch/lightswitch.js
+++ core/src/main/resources/META-INF/resources/primefaces-extensions/lightswitch/lightswitch.js
@@ -58,19 +58,22 @@ PrimeFaces.widget.ExtLightSwitch = PrimeFaces.widget.BaseWidget.extend({
      */
     changeTheme : function(theme) {
         if (theme !== this.selected) {
+            this.selected = theme;
             var options = { params: [ {name: this.id + '_theme', value: theme} ] };
             if (this.hasBehavior('switch')) {
               this.callBehavior('switch', options);
+              PrimeFaces.changeTheme(theme);
             }
             else {
               options.source = this.id;
               options.process = this.id;
               options.update = this.id;
               options.formId = this.formId;
+              options.oncomplete = function(){
+                PrimeFaces.changeTheme(theme);
+              }
               PrimeFaces.ajax.Request.handle(options);
             }            
-            PrimeFaces.changeTheme(theme);
-            this.selected = theme;
         }
     },
 
