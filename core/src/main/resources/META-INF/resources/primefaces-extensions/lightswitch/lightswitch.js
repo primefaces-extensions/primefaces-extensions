e30cef3babd1180596f64a766596668f4b183345
melloware <mellowaredev@gmail.com>
2021-03-02 08:41:07 -0500

    Fix #324: Lightswitch with CombinedResourceHandler

--- core/src/main/resources/META-INF/resources/primefaces-extensions/lightswitch/lightswitch.js
+++ core/src/main/resources/META-INF/resources/primefaces-extensions/lightswitch/lightswitch.js
@@ -57,6 +57,7 @@ PrimeFaces.widget.ExtLightSwitch = PrimeFaces.widget.BaseWidget.extend({
      * @param {string} theme to change to.
      */
     changeTheme: function (theme) {
+        var $this = this;
         if (theme !== this.selected) {
             this.selected = theme;
             var options = {params: [{name: this.id + '_theme', value: theme}]};
@@ -64,22 +65,32 @@ PrimeFaces.widget.ExtLightSwitch = PrimeFaces.widget.BaseWidget.extend({
                 this.callBehavior('switch', options);
 
                 // if theme link can't be detected just reload the window
-                var themeLink = PrimeFaces.getThemeLink();
-                if (!themeLink || themeLink.length === 0) {
-                    window.location.reload();
-                } else {
-                    PrimeFaces.changeTheme(theme);
-                }
+                $this._execute(theme);
             } else {
                 options.source = this.id;
                 options.process = this.id;
                 options.update = this.id;
                 options.formId = this.formId;
                 options.oncomplete = function () {
-                    PrimeFaces.changeTheme(theme);
+                    $this._execute(theme);
                 }
                 PrimeFaces.ajax.Request.handle(options);
             }
+        }
+    },
+
+    /**
+     * Change the theme but if using OmniFaces CombinedResourceHandler we need to reload the widow instead.
+     *
+     * @param {string} theme to change to.
+     * @private
+     */
+    _execute: function (theme) {
+        var themeLink = PrimeFaces.getThemeLink();
+        if (!themeLink || themeLink.length === 0) {
+            window.location.reload();
+        } else {
+            PrimeFaces.changeTheme(theme);
         }
     }
 
