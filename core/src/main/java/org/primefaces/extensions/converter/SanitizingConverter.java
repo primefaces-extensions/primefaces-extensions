45aa36eff5f5f308fe16a2c8789dd7a8344c2285
melloware <mellowaredev@gmail.com>
2021-08-27 10:51:36 -0400

    Fix #519: Add SanitizingConverter for pe:sanitizer

---
+++ core/src/main/java/org/primefaces/extensions/converter/SanitizingConverter.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2011-2021 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.converter;
+
+import java.io.Serializable;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import org.primefaces.util.Constants;
+import org.primefaces.util.LangUtils;
+
+/**
+ * {@link Converter} which sanitizes any input using an OWASP Java HTML Sanitizer PolicyFactory. Useful for cleansing input if going to be displayed in
+ * outputText with escape="false".
+ *
+ * @since 10.0.5
+ */
+@FacesConverter(value = "primefaces.SanitizingConverter")
+public class SanitizingConverter implements Converter<Object>, Serializable {
+
+    private static final long serialVersionUID = 20121214L;
+
+    /**
+     * Default policy blocks all HTML.
+     */
+    private static final org.owasp.html.PolicyFactory DEFAULT_POLICY = new org.owasp.html.HtmlPolicyBuilder().toFactory();
+
+    /**
+     * Custom policy provided by user.
+     */
+    private org.owasp.html.PolicyFactory policy;
+
+    /**
+     * If true use the OWASP HTML Decode to remove
+     */
+    private boolean decodeHtml = true;
+
+    /**
+     * Method to facilitate "mis-using" this class to sanitize data coming over the network
+     *
+     * @param value the value to sanitize
+     * @return sanitized string
+     */
+    public String sanitize(String value) {
+        if (LangUtils.isValueBlank(value)) {
+            return value;
+        }
+        String result = getPolicy().sanitize(value);
+        if (isDecodeHtml()) {
+            result = org.owasp.html.Encoding.decodeHtml(result);
+        }
+        return result.trim();
+    }
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+        return value == null ? null : sanitize(value);
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        return o == null ? Constants.EMPTY_STRING : sanitize(o.toString());
+    }
+
+    public org.owasp.html.PolicyFactory getPolicy() {
+        if (policy == null) {
+            policy = DEFAULT_POLICY;
+        }
+        return policy;
+    }
+
+    public void setPolicy(org.owasp.html.PolicyFactory policy) {
+        this.policy = policy;
+    }
+
+    public boolean isDecodeHtml() {
+        return decodeHtml;
+    }
+
+    public void setDecodeHtml(boolean decodeHtml) {
+        this.decodeHtml = decodeHtml;
+    }
+
+}
