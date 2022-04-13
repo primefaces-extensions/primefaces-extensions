72089ee6366253c1534533444a740891151426cb
Jasper de Vries <jepsar@gmail.com>
2022-04-13 20:11:14 +0200

    #773 Localized

---
+++ core/src/main/java/org/primefaces/extensions/component/localized/Localized.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2011-2022 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.component.localized;
+
+import java.util.Locale;
+
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.util.LocaleUtils;
+
+/**
+ * <code>Localized</code> component.
+ *
+ * @author Jasper de Vries &lt;jepsar@gmail.com&gt;
+ * @since 11.0.3
+ */
+public class Localized extends UIComponentBase {
+
+    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.Localized";
+    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
+    public static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.LocalizedRenderer";
+
+    // @formatter:off
+    @SuppressWarnings("java:S115")
+    public enum PropertyKeys {
+        name,
+        folder,
+        locale,
+        escape,
+        evalEl,
+        markdown
+    }
+    // @formatter:on
+
+    public Localized() {
+        setRendererType(DEFAULT_RENDERER);
+    }
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public Locale calculateLocale(FacesContext facesContext) {
+        return LocaleUtils.resolveLocale(facesContext, getLocale(), getClientId(facesContext));
+    }
+
+    public String getName() {
+        return (String) getStateHelper().eval(PropertyKeys.name, null);
+    }
+
+    public void setName(final String name) {
+        getStateHelper().put(PropertyKeys.name, name);
+    }
+
+    public String getFolder() {
+        return (String) getStateHelper().eval(PropertyKeys.folder, null);
+    }
+
+    public void setFolder(final String folder) {
+        getStateHelper().put(PropertyKeys.folder, folder);
+    }
+
+    public Object getLocale() {
+        return getStateHelper().eval(PropertyKeys.locale, null);
+    }
+
+    public void setLocale(final Object locale) {
+        getStateHelper().put(PropertyKeys.locale, locale);
+    }
+
+    public boolean isEscape() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.escape, true);
+    }
+
+    public void setEscape(final boolean escape) {
+        getStateHelper().put(PropertyKeys.escape, escape);
+    }
+
+    public boolean isEvalEl() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.evalEl, false);
+    }
+
+    public void setEvalEl(final boolean evalEl) {
+        getStateHelper().put(PropertyKeys.evalEl, evalEl);
+    }
+
+    public boolean isMarkdown() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.markdown, false);
+    }
+
+    public void setMarkdown(final boolean markdown) {
+        getStateHelper().put(PropertyKeys.markdown, markdown);
+    }
+
+}
---
+++ core/src/main/java/org/primefaces/extensions/component/localized/LocalizedRenderer.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2011-2022 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.component.localized;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Locale;
+
+import javax.el.ExpressionFactory;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+import org.primefaces.extensions.config.PrimeExtensionsEnvironment;
+import org.primefaces.extensions.util.CommonMarkWrapper;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.EscapeUtils;
+import org.primefaces.util.LangUtils;
+
+/**
+ * Renderer for the {@link Localized} component.
+ *
+ * @author Jasper de Vries &lt;jepsar@gmail.com&gt;
+ * @since 11.0.3
+ */
+public class LocalizedRenderer extends CoreRenderer {
+
+    public static final String FOLDER = "WEB-INF/pfe-localized";
+
+    @Override
+    public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
+        final Localized localized = (Localized) component;
+        encodeMarkup(context, localized);
+    }
+
+    protected void encodeMarkup(final FacesContext context, final Localized localized) throws IOException {
+        String value = Files.readString(resolve(context, localized));
+        if (localized.isEvalEl()) {
+            value = evaluateEl(context, value);
+        }
+        if (localized.isEscape()) {
+            value = EscapeUtils.forHtml(value);
+        }
+        if (localized.isMarkdown()) {
+            value = toHTML(context, value);
+        }
+        context.getResponseWriter().append(value);
+    }
+
+    protected Path resolve(final FacesContext context, final Localized localized) {
+        final String base = ((ServletContext) context.getExternalContext().getContext()).getRealPath(FOLDER);
+        final Locale locale = localized.calculateLocale(context);
+        final String language = locale.getLanguage();
+        final String country = locale.getCountry();
+        final String folder = localized.getFolder();
+        final String name = localized.getName();
+        Path path = resolve(base, folder, name, language, country);
+        if (path == null) {
+            path = resolve(base, null, name, language, country);
+        }
+        if (path == null) {
+            throw new IllegalStateException("Cannot find Localized file for: " + localized.getClientId(context));
+        }
+        return path;
+    }
+
+    protected Path resolve(final String base, final String folder, final String name, final String language, final String country) {
+        final String baseFolder = LangUtils.isBlank(folder)
+                    ? base
+                    : base + "/" + folder;
+        Path path = existingPath(baseFolder, name + "_" + language + "_" + country);
+        if (path == null) {
+            path = existingPath(baseFolder, name + "_" + language);
+        }
+        if (path == null) {
+            path = existingPath(baseFolder, name);
+        }
+        return path;
+    }
+
+    protected Path existingPath(final String first, final String... more) {
+        final Path path = Path.of(first, more);
+        return path.toFile().exists() ? path : null;
+    }
+
+    protected String toHTML(final FacesContext context, final String value) {
+        if (!PrimeExtensionsEnvironment.getCurrentInstance(context).isCommonmarkAvailable()) {
+            throw new FacesException("CommonMark not available.");
+        }
+        return CommonMarkWrapper.toHTML(value);
+    }
+
+    protected String evaluateEl(final FacesContext context, final String value) {
+        return (String) ExpressionFactory.newInstance()
+                    .createValueExpression(context.getELContext(), value, String.class)
+                    .getValue(context.getELContext());
+    }
+
+}
