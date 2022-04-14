eda1432a81fbd8016628c037542814d63f4d7ee0
melloware <mellowaredev@gmail.com>
2022-04-14 10:31:35 -0400

    Fix #804: CKEditor modal dialog issue

--- core/src/main/java/org/primefaces/extensions/component/ckeditor/CKEditor.java
+++ core/src/main/java/org/primefaces/extensions/component/ckeditor/CKEditor.java
@@ -41,6 +41,7 @@ import org.primefaces.util.LangUtils;
 @ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js")
 @ResourceDependency(library = "primefaces", name = "core.js")
 @ResourceDependency(library = "primefaces-extensions", name = "primefaces-extensions.js")
+@ResourceDependency(library = "primefaces-extensions", name = "ckeditor/ckeditor.css")
 @ResourceDependency(library = "primefaces-extensions", name = "ckeditor/ckeditor-widget.js")
 public class CKEditor extends AbstractPrimeHtmlInputTextArea implements ClientBehaviorHolder, Widget {
 
