55d3057d0eb200cebf8f1bbd9a989a51c37a72e4
melloware <mellowaredev@gmail.com>
2022-04-27 16:17:17 -0400

    Fix #761: CKEditor customConfig example

--- showcase/src/main/webapp/resources/js/ckeditorExtender.js
+++ showcase/src/main/webapp/resources/js/ckeditorExtender.js
@@ -3,6 +3,6 @@ CKEDITOR.editorConfig = function (config) {
     config.toolbar = [
         {name: 'colors', items: ['TextColor', 'BGColor']},
         {name: 'links', items: ['Link', 'Unlink']},
-        {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+        {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']}
     ];
 };
