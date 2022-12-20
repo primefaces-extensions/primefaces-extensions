5f5f3373362048414af1fcb113b8cebac1f65d02
melloware <mellowaredev@gmail.com>
2022-12-20 17:24:37 -0500

    Revert "Fix #1011: JSF23 wrapped constructors"
    This reverts commit 834049aa3780563ef6e1e3111b6a1406e131e395.

--- core/src/main/java/org/primefaces/extensions/application/PrimeFacesExtensionsResource.java
+++ core/src/main/java/org/primefaces/extensions/application/PrimeFacesExtensionsResource.java
@@ -57,4 +57,38 @@ public class PrimeFacesExtensionsResource extends ResourceWrapper {
         return super.getRequestPath() + version;
     }
 
+    @Override
+    public String getContentType() {
+        return getWrapped().getContentType();
+    }
+
+    @Override
+    public String getLibraryName() {
+        return getWrapped().getLibraryName();
+    }
+
+    @Override
+    public String getResourceName() {
+        return getWrapped().getResourceName();
+    }
+
+    @Override
+    public void setContentType(final String contentType) {
+        getWrapped().setContentType(contentType);
+    }
+
+    @Override
+    public void setLibraryName(final String libraryName) {
+        getWrapped().setLibraryName(libraryName);
+    }
+
+    @Override
+    public void setResourceName(final String resourceName) {
+        getWrapped().setResourceName(resourceName);
+    }
+
+    @Override
+    public String toString() {
+        return getWrapped().toString();
+    }
 }
