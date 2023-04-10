b06e55f916781f840cdda959b47cdd7b4aeaa65f
Andre Wachsmuth <sensenmann5@gmail.com>
2023-04-10 18:24:27 +0100

    Adjust imports for mkdirp, which removed the default export from its API

--- monacoeditor/src/main/js/src/util/java-types.js
+++ monacoeditor/src/main/js/src/util/java-types.js
@@ -3,7 +3,7 @@
 import { promises as fs } from "node:fs";
 import { join } from "node:path";
 import rimraf from "rimraf";
-import mkdirp from "mkdirp";
+import { mkdirp } from "mkdirp";
 
 import { javaDescriptorPackage, javaDescriptorPath } from "./paths.js";
 
