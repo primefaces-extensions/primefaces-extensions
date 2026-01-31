8948a7b2ae6c16a9ef517029995e1d0ed3550a79
jxmai <jxmai@users.noreply.github.com>
2026-01-30 21:50:24 -0600

    Fix #1050: OrgChart: FontAwesome icons no longer work
    - Map parentNodeSymbol to icons.parentNode in the widget JS
    - Set icons.theme based on icon prefix (fa- for FontAwesome, oci- for others)

--- core/src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
+++ core/src/main/resources/META-INF/resources/primefaces-extensions/orgchart/3-orgchart-widget.js
@@ -29,6 +29,18 @@ PrimeFaces.widget.ExtOrgChart = class extends PrimeFaces.widget.BaseWidget {
         var opts = $.extend(true, {}, cfg);
         opts['data'] = JSON.parse(opts['data']);
 
+        // Map parentNodeSymbol to icons.parentNode
+        if (opts.parentNodeSymbol) {
+            opts.icons = opts.icons || {};
+            opts.icons.parentNode = opts.parentNodeSymbol;
+            // Set theme based on icon prefix
+            if (opts.parentNodeSymbol.startsWith('fa-')) {
+                opts.icons.theme = 'fa';
+            } else if (opts.parentNodeSymbol.startsWith('oci-')) {
+                opts.icons.theme = 'oci';
+            }
+        }
+
         this.orgchart = this.jq.orgchart(opts);
 
         this._bindEvents();
