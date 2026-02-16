c607f9296f306e27d0384114c314ef499781602b
jxmai <jxmai@users.noreply.github.com>
2026-02-16 09:19:01 -0600

    Update imports from jakarta to javax in AcrossElementsMarkTextController for PF 15 compatibility

--- showcase/src/main/java/org/primefaces/extensions/showcase/controller/marktext/AcrossElementsMarkTextController.java
+++ showcase/src/main/java/org/primefaces/extensions/showcase/controller/marktext/AcrossElementsMarkTextController.java
@@ -23,8 +23,8 @@ package org.primefaces.extensions.showcase.controller.marktext;
 
 import java.io.Serializable;
 
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 /**
  * Across Elements MarkText Controller.
