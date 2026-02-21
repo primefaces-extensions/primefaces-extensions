846d16757ebdef8f61439e138fe2475f6aa72942
Melloware <mellowaredev@gmail.com>
2026-02-21 11:40:57 -0500

    CDK: FuzzySearch, GChart, Head (#2323)

--- core/src/main/java/org/primefaces/extensions/component/gchart/GChart.java
+++ core/src/main/java/org/primefaces/extensions/component/gchart/GChart.java
@@ -21,150 +21,54 @@
  */
 package org.primefaces.extensions.component.gchart;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 
 import jakarta.faces.application.ResourceDependency;
-import jakarta.faces.component.UIOutput;
-import jakarta.faces.component.behavior.ClientBehaviorHolder;
+import jakarta.faces.component.FacesComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.event.AjaxBehaviorEvent;
 import jakarta.faces.event.FacesEvent;
 
-import org.primefaces.component.api.Widget;
+import org.primefaces.cdk.api.FacesComponentInfo;
 import org.primefaces.event.SelectEvent;
+import org.primefaces.extensions.util.Constants;
 import org.primefaces.extensions.util.json.GsonConverter;
-import org.primefaces.util.Constants;
 
+import com.google.gson.JsonArray;
+
+/**
+ * <code>GChart</code> component (Google Charts).
+ */
+@FacesComponent(value = GChart.COMPONENT_TYPE, namespace = GChart.COMPONENT_FAMILY)
+@FacesComponentInfo(description = "GChart is a Google Charts visualization component.")
 @ResourceDependency(library = "primefaces", name = "jquery/jquery.js")
 @ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js")
 @ResourceDependency(library = "primefaces", name = "core.js")
-@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "primefaces-extensions.js")
-@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "gchart/gchart.css")
-@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "gchart/gchart.js")
-public class GChart extends UIOutput implements Widget, ClientBehaviorHolder {
+@ResourceDependency(library = Constants.LIBRARY, name = "primefaces-extensions.js")
+@ResourceDependency(library = Constants.LIBRARY, name = "gchart/gchart.css")
+@ResourceDependency(library = Constants.LIBRARY, name = "gchart/gchart.js")
+public class GChart extends GChartBaseImpl {
 
+    /** Init parameter name for Google Maps/Charts API key. */
     public static final String API_KEY = "primefaces.GOOGLE_MAPS_API_KEY";
-
-    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.GChart";
-    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
-    private static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.GChartRenderer";
-
-    private static final String DEFAULT_EVENT = "select";
-    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList(DEFAULT_EVENT));
-
-    @SuppressWarnings("java:S115")
-    protected enum PropertyKeys {
-        widgetVar, width, height, title, apiKey, language, extender
-    }
-
-    public GChart() {
-        setRendererType(DEFAULT_RENDERER);
-    }
-
-    @Override
-    public String getFamily() {
-        return COMPONENT_FAMILY;
-    }
-
-    @Override
-    public Collection<String> getEventNames() {
-        return EVENT_NAMES;
-    }
-
-    @Override
-    public String getDefaultEventName() {
-        return DEFAULT_EVENT;
-    }
-
-    public String getWidgetVar() {
-        return (String) getStateHelper().eval(PropertyKeys.widgetVar, null);
-    }
-
-    public void setWidgetVar(final String _widgetVar) {
-        getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
-    }
-
-    public Integer getWidth() {
-        return (Integer) getStateHelper().eval(PropertyKeys.width, null);
-    }
-
-    public void setWidth(final Integer width) {
-        getStateHelper().put(PropertyKeys.width, width);
-    }
-
-    public Integer getHeight() {
-        return (Integer) getStateHelper().eval(PropertyKeys.height, null);
-    }
-
-    public void setHeight(final Integer width) {
-        getStateHelper().put(PropertyKeys.height, width);
-    }
-
-    public String getTitle() {
-        return (String) getStateHelper().eval(PropertyKeys.title, null);
-    }
-
-    public void setTitle(final String title) {
-        getStateHelper().put(PropertyKeys.title, title);
-    }
-
-    public String getApiKey() {
-        return (String) getStateHelper().eval(PropertyKeys.apiKey, null);
-    }
-
-    public void setApiKey(final String apiKey) {
-        getStateHelper().put(PropertyKeys.apiKey, apiKey);
-    }
-
-    public String getLanguage() {
-        return (String) getStateHelper().eval(PropertyKeys.language, "en");
-    }
-
-    public void setLanguage(final String language) {
-        getStateHelper().put(PropertyKeys.language, language);
-    }
-
-    public String getExtender() {
-        return (String) getStateHelper().eval(PropertyKeys.extender, null);
-    }
-
-    public void setExtender(final String extender) {
-        getStateHelper().put(PropertyKeys.extender, extender);
-    }
 
     @Override
     public void queueEvent(final FacesEvent event) {
-
-        final FacesContext context = getFacesContext();
-        if (isRequestSource(context) && event instanceof AjaxBehaviorEvent) {
+        if (isAjaxBehaviorEventSource(event)) {
+            final FacesContext context = event.getFacesContext();
             final Map<String, String> params = context.getExternalContext().getRequestParameterMap();
-            final String eventName = params.get(Constants.RequestParams.PARTIAL_BEHAVIOR_EVENT_PARAM);
+            final AjaxBehaviorEvent behaviorEvent = (AjaxBehaviorEvent) event;
 
-            if (eventName.equals(DEFAULT_EVENT)) {
-                final AjaxBehaviorEvent behaviorEvent = (AjaxBehaviorEvent) event;
+            if (isAjaxBehaviorEvent(event, ClientBehaviorEventKeys.select)) {
                 final String clientId = getClientId(context);
-
                 final Object value = GsonConverter.getGson().fromJson(params.get(clientId + "_hidden"),
-                            com.google.gson.JsonArray.class);
-
-                final SelectEvent selectEvent = new SelectEvent(this, behaviorEvent.getBehavior(), value);
+                            JsonArray.class);
+                final SelectEvent<Object> selectEvent = new SelectEvent<>(this, behaviorEvent.getBehavior(), value);
                 selectEvent.setPhaseId(behaviorEvent.getPhaseId());
-
                 super.queueEvent(selectEvent);
+                return;
             }
         }
-        else {
-            super.queueEvent(event);
-        }
-    }
-
-    public boolean isRequestSource(final FacesContext context) {
-        final String partialSource = context.getExternalContext().getRequestParameterMap()
-                    .get(Constants.RequestParams.PARTIAL_SOURCE_PARAM);
-
-        return getClientId(context).equals(partialSource);
+        super.queueEvent(event);
     }
 }
---
+++ core/src/main/java/org/primefaces/extensions/component/gchart/GChartBase.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2011-2026 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.component.gchart;
+
+import jakarta.faces.component.UIOutput;
+
+import org.primefaces.cdk.api.FacesBehaviorEvent;
+import org.primefaces.cdk.api.FacesBehaviorEvents;
+import org.primefaces.cdk.api.FacesComponentBase;
+import org.primefaces.cdk.api.Property;
+import org.primefaces.component.api.Widget;
+import org.primefaces.event.SelectEvent;
+
+/**
+ * <code>GChart</code> component base class (Google Charts).
+ */
+@FacesComponentBase
+@FacesBehaviorEvents({
+            @FacesBehaviorEvent(name = "select", event = SelectEvent.class,
+                        description = "Fires when a chart element is selected.", defaultEvent = true)
+})
+public abstract class GChartBase extends UIOutput implements Widget {
+
+    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.GChart";
+    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
+    public static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.GChartRenderer";
+
+    public GChartBase() {
+        setRendererType(DEFAULT_RENDERER);
+    }
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    @Property(description = "Chart width in pixels.")
+    public abstract Integer getWidth();
+
+    @Property(description = "Chart height in pixels.")
+    public abstract Integer getHeight();
+
+    @Property(description = "Chart title.")
+    public abstract String getTitle();
+
+    @Property(description = "Google Charts API key.")
+    public abstract String getApiKey();
+
+    @Property(description = "Language code for the chart.", defaultValue = "en")
+    public abstract String getLanguage();
+
+    @Property(description = "JavaScript extender function name.")
+    public abstract String getExtender();
+}
