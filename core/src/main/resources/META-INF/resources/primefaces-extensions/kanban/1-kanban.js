5f216e347ce52871edb92e84841cc7203a98dc16
jxmai <jxmai@users.noreply.github.com>
2026-05-19 09:09:08 -0500

    PF 15 Backport #1980: Kanban: New Component

---
+++ core/src/main/resources/META-INF/resources/primefaces-extensions/kanban/1-kanban.js
@@ -0,0 +1,103 @@
+/**
+ * PrimeFaces Extensions Kanban Widget
+ * Copyright (c) 2011-2026 PrimeFaces Extensions
+ */
+if (PrimeFaces.widget) {
+    PrimeFaces.widget.ExtKanban = class extends PrimeFaces.widget.DeferredWidget {
+
+        init(cfg) {
+            super.init(cfg);
+            this.cfg = cfg;
+            this.boards = typeof cfg.boards === 'string' ? JSON.parse(cfg.boards) : (cfg.boards || []);
+
+            if (this.cfg.extender && typeof this.cfg.extender === 'function') {
+                this.cfg.extender.call(this);
+            }
+
+            this.renderDeferred();
+        }
+
+        _render() {
+            var $this = this;
+
+            var options = {
+                element: PrimeFaces.escapeClientId(this.id),
+                boards: this.boards,
+                dragItems: this.cfg.draggable !== false,
+                dragBoards: false,
+                itemAddOptions: {
+                    enabled: this.cfg.addItemButton || false,
+                    content: '+ Add Item',
+                    footer: false
+                },
+                dropEl: function(el, target, source, sibling) {
+                    var itemId = el.getAttribute('data-eid');
+                    var sourceBoard = source.closest('.kanban-board');
+                    var targetBoard = target.closest('.kanban-board');
+                    var sourceId = sourceBoard ? sourceBoard.getAttribute('data-id') : null;
+                    var targetId = targetBoard ? targetBoard.getAttribute('data-id') : null;
+                    var targetIndex = Array.prototype.slice.call(target.children).indexOf(el);
+                    $this.onDrop(itemId, sourceId, targetId, targetIndex);
+                },
+                buttonClick: function(el, boardId) {
+                    $this.onAddItem(boardId);
+                }
+            };
+
+            this.instance = new jKanban(options);
+        }
+
+        onDrop(itemId, sourceColumnId, targetColumnId, newPosition) {
+            if (this.hasBehavior('drop')) {
+                this.callBehavior('drop', {
+                    source: this.id,
+                    process: this.id,
+                    params: [
+                        {name: this.id + '_itemId', value: itemId},
+                        {name: this.id + '_sourceColumnId', value: sourceColumnId},
+                        {name: this.id + '_targetColumnId', value: targetColumnId},
+                        {name: this.id + '_newPosition', value: newPosition}
+                    ]
+                });
+            }
+        }
+
+        onAddItem(columnId) {
+            if (this.hasBehavior('itemAdd')) {
+                this.callBehavior('itemAdd', {
+                    source: this.id,
+                    process: this.id,
+                    params: [
+                        {name: this.id + '_columnId', value: columnId}
+                    ]
+                });
+            }
+        }
+
+        refresh(cfg) {
+            this._cleanup();
+            super.refresh(cfg);
+        }
+
+        destroy() {
+            this._cleanup();
+            super.destroy();
+        }
+
+        _cleanup() {
+            if (this.instance) {
+                if (this.instance.drake && this.instance.drake.destroy) {
+                    this.instance.drake.destroy();
+                }
+                if (this.instance.drakeBoard && this.instance.drakeBoard.destroy) {
+                    this.instance.drakeBoard.destroy();
+                }
+                this.instance = null;
+                var container = document.getElementById(this.id);
+                if (container) {
+                    container.innerHTML = '';
+                }
+            }
+        }
+    };
+}
---
+++ showcase/src/main/java/org/primefaces/extensions/showcase/controller/KanbanController.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2011-2025 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.showcase.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.extensions.event.KanbanAddEvent;
+import org.primefaces.extensions.event.KanbanDragEvent;
+import org.primefaces.extensions.model.kanban.KanbanColumn;
+import org.primefaces.extensions.model.kanban.KanbanItem;
+
+/**
+ * KanbanController
+ *
+ * @author jxmai
+ * @since 15.0.16
+ */
+@Named
+@ViewScoped
+public class KanbanController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<KanbanColumn> columns;
+
+    @PostConstruct
+    public void init() {
+        columns = new ArrayList<>();
+
+        KanbanColumn todoColumn = new KanbanColumn("todo", "To Do");
+        todoColumn.setCssClass("kanban-todo");
+        todoColumn.addItem(new KanbanItem("item1", "Add support for Kanban",
+                    "Create a new Kanban component for PrimeFaces Extensions"));
+        todoColumn.addItem(new KanbanItem("item2", "Write documentation",
+                    "Document the new component"));
+
+        KanbanColumn inProgressColumn = new KanbanColumn("inprogress", "In Progress");
+        inProgressColumn.setCssClass("kanban-inprogress");
+        inProgressColumn.addItem(new KanbanItem("item3", "Design component",
+                    "Design the Kanban board component"));
+
+        KanbanColumn doneColumn = new KanbanColumn("done", "Done");
+        doneColumn.setCssClass("kanban-done");
+        doneColumn.addItem(new KanbanItem("item4", "Research libraries",
+                    "Research available Kanban libraries"));
+
+        columns.add(todoColumn);
+        columns.add(inProgressColumn);
+        columns.add(doneColumn);
+    }
+
+    public void onDrop(KanbanDragEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Moved",
+                    "Item " + event.getItemId() + " moved from " + event.getSourceColumnId()
+                                + " to " + event.getTargetColumnId() + " at position " + event.getNewPosition());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void onAddItem(KanbanAddEvent event) {
+        for (KanbanColumn column : columns) {
+            if (column.getId().equals(event.getColumnId())) {
+                int count = column.getItems().size() + 1;
+                column.addItem(new KanbanItem("new-" + count, "New Task " + count,
+                            "Click to edit this task"));
+                break;
+            }
+        }
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Added",
+                    "New item added to column " + event.getColumnId());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public List<KanbanColumn> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<KanbanColumn> columns) {
+        this.columns = columns;
+    }
+}
