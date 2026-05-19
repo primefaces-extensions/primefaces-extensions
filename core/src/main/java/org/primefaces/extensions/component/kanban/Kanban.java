0f78502381e96ce67b27e10099cf2263274e6b9c
jxmai <jxmai@users.noreply.github.com>
2026-05-19 09:52:14 -0500

    PF 15 Backport #1980: Kanban: New Component (#2524)

---
+++ core/src/main/java/org/primefaces/extensions/component/kanban/Kanban.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2011-2025 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.component.kanban;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.behavior.ClientBehaviorHolder;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.FacesEvent;
+
+import org.primefaces.extensions.event.KanbanAddEvent;
+import org.primefaces.extensions.event.KanbanDragEvent;
+import org.primefaces.util.Constants;
+
+/**
+ * <code>Kanban</code> component.
+ *
+ * @author jxmai
+ * @since 15.0.16
+ */
+@ResourceDependency(library = "primefaces", name = "jquery/jquery.js")
+@ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js")
+@ResourceDependency(library = "primefaces", name = "core.js")
+@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "primefaces-extensions.js")
+@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "kanban/kanban.css")
+@ResourceDependency(library = org.primefaces.extensions.util.Constants.LIBRARY, name = "kanban/kanban.js")
+public class Kanban extends KanbanBase implements ClientBehaviorHolder {
+
+    private static final Collection<String> EVENT_NAMES = Collections
+                .unmodifiableCollection(Arrays.asList(KanbanDragEvent.NAME, KanbanAddEvent.NAME));
+
+    @Override
+    public Collection<String> getEventNames() {
+        return EVENT_NAMES;
+    }
+
+    @Override
+    public String getDefaultEventName() {
+        return KanbanDragEvent.NAME;
+    }
+
+    @Override
+    public void processDecodes(final FacesContext fc) {
+        if (isSelfRequest(fc)) {
+            decode(fc);
+        }
+        else {
+            super.processDecodes(fc);
+        }
+    }
+
+    @Override
+    public void queueEvent(final FacesEvent event) {
+        final FacesContext fc = FacesContext.getCurrentInstance();
+
+        if (isSelfRequest(fc) && event instanceof AjaxBehaviorEvent) {
+            final Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+            final String eventName = params.get(Constants.RequestParams.PARTIAL_BEHAVIOR_EVENT_PARAM);
+            final AjaxBehaviorEvent behaviorEvent = (AjaxBehaviorEvent) event;
+            final String clientId = getClientId(fc);
+
+            if (KanbanDragEvent.NAME.equals(eventName)) {
+                final String itemId = params.get(clientId + "_itemId");
+                final String sourceColumnId = params.get(clientId + "_sourceColumnId");
+                final String targetColumnId = params.get(clientId + "_targetColumnId");
+                final String positionStr = params.get(clientId + "_newPosition");
+                final int newPosition = parsePosition(positionStr);
+
+                final KanbanDragEvent kanbanDragEvent = new KanbanDragEvent(this,
+                            behaviorEvent.getBehavior(), itemId, sourceColumnId, targetColumnId, newPosition);
+                kanbanDragEvent.setPhaseId(behaviorEvent.getPhaseId());
+                super.queueEvent(kanbanDragEvent);
+                return;
+            }
+
+            if (KanbanAddEvent.NAME.equals(eventName)) {
+                final String columnId = params.get(clientId + "_columnId");
+                final KanbanAddEvent addEvent = new KanbanAddEvent(this,
+                            behaviorEvent.getBehavior(), columnId);
+                addEvent.setPhaseId(behaviorEvent.getPhaseId());
+                super.queueEvent(addEvent);
+                return;
+            }
+        }
+
+        super.queueEvent(event);
+    }
+
+    private boolean isSelfRequest(final FacesContext context) {
+        return getClientId(context)
+                    .equals(context.getExternalContext().getRequestParameterMap().get(
+                                Constants.RequestParams.PARTIAL_SOURCE_PARAM));
+    }
+
+    private int parsePosition(final String positionStr) {
+        if (positionStr != null) {
+            try {
+                return Integer.parseInt(positionStr);
+            }
+            catch (final NumberFormatException e) {
+                // fallback to default position 0 on malformed input
+            }
+        }
+        return 0;
+    }
+}
---
+++ core/src/main/java/org/primefaces/extensions/component/kanban/KanbanBase.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2011-2025 PrimeFaces Extensions
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.primefaces.extensions.component.kanban;
+
+import java.util.List;
+
+import javax.faces.component.UIComponentBase;
+
+import org.primefaces.component.api.Widget;
+
+/**
+ * Component base class for the <code>Kanban</code> component.
+ *
+ * @author jxmai
+ * @since 15.0.16
+ */
+public abstract class KanbanBase extends UIComponentBase implements Widget {
+
+    public static final String COMPONENT_TYPE = "org.primefaces.extensions.component.Kanban";
+    public static final String COMPONENT_FAMILY = "org.primefaces.extensions.component";
+    public static final String DEFAULT_RENDERER = "org.primefaces.extensions.component.kanban.KanbanRenderer";
+
+    public static final String STYLE_CLASS = "ui-kanban";
+
+    @SuppressWarnings("java:S115")
+    protected enum PropertyKeys {
+        value, style, styleClass, draggable, addItemButton, extender
+    }
+
+    public KanbanBase() {
+        setRendererType(DEFAULT_RENDERER);
+    }
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public List getValue() {
+        return (List) getStateHelper().eval(PropertyKeys.value, null);
+    }
+
+    public void setValue(List value) {
+        getStateHelper().put(PropertyKeys.value, value);
+    }
+
+    public String getStyle() {
+        return (String) getStateHelper().eval(PropertyKeys.style, null);
+    }
+
+    public void setStyle(String style) {
+        getStateHelper().put(PropertyKeys.style, style);
+    }
+
+    public String getStyleClass() {
+        return (String) getStateHelper().eval(PropertyKeys.styleClass, null);
+    }
+
+    public void setStyleClass(String styleClass) {
+        getStateHelper().put(PropertyKeys.styleClass, styleClass);
+    }
+
+    public boolean isDraggable() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.draggable, true);
+    }
+
+    public void setDraggable(boolean draggable) {
+        getStateHelper().put(PropertyKeys.draggable, draggable);
+    }
+
+    public boolean isAddItemButton() {
+        return (Boolean) getStateHelper().eval(PropertyKeys.addItemButton, false);
+    }
+
+    public void setAddItemButton(boolean addItemButton) {
+        getStateHelper().put(PropertyKeys.addItemButton, addItemButton);
+    }
+
+    public String getExtender() {
+        return (String) getStateHelper().eval(PropertyKeys.extender, null);
+    }
+
+    public void setExtender(String extender) {
+        getStateHelper().put(PropertyKeys.extender, extender);
+    }
+}
