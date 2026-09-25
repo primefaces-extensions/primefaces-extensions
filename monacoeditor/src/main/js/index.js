5683e9b43c218d378192d3cceff85cd15100224c
jxmai <jxmai@users.noreply.github.com>
2026-09-25 07:37:52 -0500

    #2789: MonacoEditor: Upgrade to Latest Version v0.57.0 (#2790)
    * #2789: MonacoEditor: Upgrade to Latest Version v0.57.0
    
    * #2789: MonacoEditor: Fix MonacoEnvironment.baseUrl for the inline loader and preserve MonacoEnvironment.baseUrl instead of overwriting it

--- monacoeditor/src/main/js/index.js
+++ monacoeditor/src/main/js/index.js
@@ -2,5 +2,8 @@
 
 import "./prepare-environment.js";
 
-export * from "monaco-editor-mod";
-export * from "monaco-editor-mod/esm/monacoExtras.js";
+import * as monaco from "monaco-editor-mod";
+
+globalThis.monaco = monaco;
+
+export * from "./node_modules/monaco-editor-mod/esm/monacoExtras.js";
